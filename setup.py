c9002e7627c7c413abf5e080ea782ee0adea6e6e
Jiri Kuncar <jiri.kuncar@gmail.com>
2020-05-25 19:52:51 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the 3-clause BSD style license (see LICENSE).
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019 Datadog, Inc.
+
+import os
+
+from setuptools import setup
+
+version_template = """\
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the 3-clause BSD style license (see LICENSE).
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019 Datadog, Inc.
+
+__version__ = "{version}"
+"""
+
+# Allows the fetching of tags even from shallow clones
+# https://github.com/pypa/setuptools_scm/pull/118#issuecomment-255381535
+def parse_fetch_on_shallow(root):
+    from setuptools_scm.git import parse, fetch_on_shallow
+
+    return parse(root, pre_parse=fetch_on_shallow)
+
+
+setup(
+    use_scm_version={
+        "local_scheme": "dirty-tag",
+        "write_to": os.path.join("datadog_api_client", "version.py"),
+        "write_to_template": version_template,
+        "parse": parse_fetch_on_shallow,
+    }
+)
