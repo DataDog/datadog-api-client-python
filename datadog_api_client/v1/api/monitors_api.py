5d58e2dda9b1733758eb22e40431fc26af7f018e
ci.datadog-api-spec <packages@datadoghq.com>
2020-06-30 21:01:05 +0000

    Regenerate client from commit 3c92af9 of spec repo

--- datadog_api_client/v1/api/monitors_api.py
+++ datadog_api_client/v1/api/monitors_api.py
@@ -558,7 +558,7 @@ class MonitorsApi(object):
                 tags (str): A comma separated list indicating what tags, if any, should be used to filter the list of monitors by scope. For example, &#x60;host:host0&#x60;.. [optional]
                 monitor_tags (str): A comma separated list indicating what service and/or custom tags, if any, should be used to filter the list of monitors. Tags created in the Datadog UI automatically have the service key prepended. For example, &#x60;service:my-app&#x60;.. [optional]
                 with_downtimes (bool): If this argument is set to true, then the returned data includes all current downtimes for each monitor.. [optional]
-                id_offset (int): The time (in seconds) to delay the monitor evaluation compared to the latest data timestamp received.. [optional]
+                id_offset (int): Monitor ID offset.. [optional]
                 page (int): The page to start paginating from. If this argument is not specified, the request returns all monitors without pagination.. [optional]
                 page_size (int): The number of monitors to return per page. If the page argument is not specified, the default behavior returns all monitors without a &#x60;page_size&#x60; limit. However, if page is specified and &#x60;page_size&#x60; is not, the argument defaults to 100.. [optional]
                 _return_http_data_only (bool): response data without head status
