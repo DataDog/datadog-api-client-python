a3cb91869dbb266cf79e30c857b429d2f192f0fb
ci.datadog-api-spec <packages@datadoghq.com>
2020-07-08 07:59:34 +0000

    Regenerate client from commit 44c5568 of spec repo

---
+++ docs/v1/SyntheticsAssertionJSONPathOperator.md
@@ -0,0 +1,11 @@
+# synthetics_assertion_json_path_operator.SyntheticsAssertionJSONPathOperator
+
+Assertion operator to apply.
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**value** | **str** | Assertion operator to apply. | defaults to 'validatesJSONPath'
+
+[[Back to Model list]](README.md#documentation-for-models) [[Back to API list]](README.md#documentation-for-api-endpoints) [[Back to README]](README.md)
+
+
---
+++ tests/v1/test_synthetics_assertion_target.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019-Present Datadog, Inc.
+
+
+from __future__ import absolute_import
+import sys
+import unittest
+
+import datadog_api_client.v1
+try:
+    from datadog_api_client.v1.model import synthetics_assertion_operator
+except ImportError:
+    synthetics_assertion_operator = sys.modules[
+        'datadog_api_client.v1.model.synthetics_assertion_operator']
+try:
+    from datadog_api_client.v1.model import synthetics_assertion_type
+except ImportError:
+    synthetics_assertion_type = sys.modules[
+        'datadog_api_client.v1.model.synthetics_assertion_type']
+from datadog_api_client.v1.model.synthetics_assertion_target import SyntheticsAssertionTarget
+
+
+class TestSyntheticsAssertionTarget(unittest.TestCase):
+    """SyntheticsAssertionTarget unit test stubs"""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testSyntheticsAssertionTarget(self):
+        """Test SyntheticsAssertionTarget"""
+        # FIXME: construct object with mandatory attributes with example values
+        # model = SyntheticsAssertionTarget()  # noqa: E501
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
