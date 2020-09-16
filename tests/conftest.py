7a11766a2c0891026183a58b5c2462f29a790ea7
Matthew Hardwick <matt.hardwick@datadoghq.com>
2020-09-16 01:21:35 -0400

    add handling in bdd runner for unstable operations (#67)
    * add handling in bdd runner for unstable operations
    
    * fix docstring

--- tests/conftest.py
+++ tests/conftest.py
@@ -279,6 +279,11 @@ def api(package_name, client, name):
         "calls": [],
     }
 
+@given(parsers.parse('operation "{name}" enabled'))
+def operation_enabled(client, name):
+    """Enable the unstable operation specific in the clause."""
+    client.configuration.unstable_operations[snake_case(name)] = True
+
 
 @given(parsers.parse('new "{name}" request'))
 def api_request(api, name):
