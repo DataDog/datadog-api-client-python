7ef4f4aef1bf4946b3262d37edefc45899737f5b
Nicholas Muesch <nicholas.muesch@datadoghq.com>
2020-10-14 10:17:21 -0400

    Update version_template to include Present (#112)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ version_template = """\
 # Unless explicitly stated otherwise all files in this repository are licensed
 # under the 3-clause BSD style license (see LICENSE).
 # This product includes software developed at Datadog (https://www.datadoghq.com/).
-# Copyright 2019 Datadog, Inc.
+# Copyright 2020-Present Datadog, Inc.
 
 __version__ = "{version}"
 """
