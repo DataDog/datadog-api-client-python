09bf33d4b46b39f88f19086f26076b58ba72114a
Sami Tabet <sami.tabet@datadoghq.com>
2021-01-28 14:26:53 +0100

    Revert "fix"
    This reverts commit f896817f2a64716b0182d88cab251b8f77c71392.

--- tests/v2/test_logs_aggregate_request.py
+++ tests/v2/test_logs_aggregate_request.py
@@ -11,10 +11,10 @@ import unittest
 
 import datadog_api_client.v2
 try:
-    from datadog_api_client.v2.model import logs_aggregate_request_page
+    from datadog_api_client.v2.model import logs_aggregate_request_paging
 except ImportError:
-    logs_aggregate_request_page = sys.modules[
-        'datadog_api_client.v2.model.logs_aggregate_request_page']
+    logs_aggregate_request_paging = sys.modules[
+        'datadog_api_client.v2.model.logs_aggregate_request_paging']
 try:
     from datadog_api_client.v2.model import logs_compute
 except ImportError:
