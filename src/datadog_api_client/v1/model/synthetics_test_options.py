be7c144a6cb27cfc8536c95d0a1f6fa766da7560
ci.datadog-api-spec <packages@datadoghq.com>
2021-06-08 14:41:18 +0000

    Regenerate client from commit e6c8320 of spec repo

--- src/datadog_api_client/v1/model/synthetics_test_options.py
+++ src/datadog_api_client/v1/model/synthetics_test_options.py
@@ -60,7 +60,12 @@ class SyntheticsTestOptions(ModelNormal):
 
     allowed_values = {}
 
-    validations = {}
+    validations = {
+        ("monitor_priority",): {
+            "inclusive_maximum": 5,
+            "inclusive_minimum": 1,
+        },
+    }
 
     additional_properties_type = None
 
@@ -85,7 +90,9 @@ class SyntheticsTestOptions(ModelNormal):
             "follow_redirects": (bool,),  # noqa: E501
             "min_failure_duration": (int,),  # noqa: E501
             "min_location_failed": (int,),  # noqa: E501
+            "monitor_name": (str,),  # noqa: E501
             "monitor_options": (SyntheticsTestOptionsMonitorOptions,),  # noqa: E501
+            "monitor_priority": (int,),  # noqa: E501
             "no_screenshot": (bool,),  # noqa: E501
             "retry": (SyntheticsTestOptionsRetry,),  # noqa: E501
             "tick_every": (SyntheticsTickInterval,),  # noqa: E501
@@ -103,7 +110,9 @@ class SyntheticsTestOptions(ModelNormal):
         "follow_redirects": "follow_redirects",  # noqa: E501
         "min_failure_duration": "min_failure_duration",  # noqa: E501
         "min_location_failed": "min_location_failed",  # noqa: E501
+        "monitor_name": "monitor_name",  # noqa: E501
         "monitor_options": "monitor_options",  # noqa: E501
+        "monitor_priority": "monitor_priority",  # noqa: E501
         "no_screenshot": "noScreenshot",  # noqa: E501
         "retry": "retry",  # noqa: E501
         "tick_every": "tick_every",  # noqa: E501
@@ -164,7 +173,9 @@ class SyntheticsTestOptions(ModelNormal):
             follow_redirects (bool): For API HTTP test, whether or not the test should follow redirects.. [optional]  # noqa: E501
             min_failure_duration (int): Minimum amount of time in failure required to trigger an alert.. [optional]  # noqa: E501
             min_location_failed (int): Minimum number of locations in failure required to trigger an alert.. [optional]  # noqa: E501
+            monitor_name (str): The monitor name is used for the alert title as well as for all monitor dashboard widgets and SLOs.. [optional]  # noqa: E501
             monitor_options (SyntheticsTestOptionsMonitorOptions): [optional]  # noqa: E501
+            monitor_priority (int): Integer from 1 (high) to 5 (low) indicating alert severity.. [optional]  # noqa: E501
             no_screenshot (bool): Prevents saving screenshots of the steps.. [optional]  # noqa: E501
             retry (SyntheticsTestOptionsRetry): [optional]  # noqa: E501
             tick_every (SyntheticsTickInterval): [optional]  # noqa: E501
