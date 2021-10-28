513ed45074df00039aee0cfa5bbf6532449ff1fb
Jiri Kuncar <jiri.kuncar@gmail.com>
2021-10-28 14:14:17 +0200

    do not use pybeta

--- check-examples.sh
+++ check-examples.sh
@@ -2,7 +2,7 @@
 
 ./extract-code-blocks.sh examples
 
-ls examples/*/*/*.pybeta | xargs -P $(($(nproc)*2)) -n 1 python -m py_compile
+ls examples/*/*/*.py | xargs -P $(($(nproc)*2)) -n 1 python -m py_compile
 if [ $? -ne 0 ]; then
     echo -e "Failed to build examples"
     exit 1
