63fa6cdcc5daaf2d4af99685bc88052c1bd194e4
ci.datadog-api-spec <packages@datadoghq.com>
2021-11-23 10:46:14 +0000

    Regenerate client from commit a3413fa of spec repo

---
+++ examples/v2/logs/ListLogs.py
@@ -0,0 +1,26 @@
+"""
+Search logs returns "OK" response
+"""
+
+from datadog_api_client.v2 import ApiClient, Configuration
+from datadog_api_client.v2.api.logs_api import LogsApi
+from datadog_api_client.v2.model.logs_list_request import LogsListRequest
+from datadog_api_client.v2.model.logs_list_request_page import LogsListRequestPage
+from datadog_api_client.v2.model.logs_query_filter import LogsQueryFilter
+from datadog_api_client.v2.model.logs_sort import LogsSort
+
+body = LogsListRequest(
+    filter=LogsQueryFilter(
+        query="datadog-agent", indexes=["main"], _from="2020-09-17T11:48:36+01:00", to="2020-09-17T12:48:36+01:00"
+    ),
+    sort=LogsSort("timestamp"),
+    page=LogsListRequestPage(limit=5),
+)
+
+configuration = Configuration()
+configuration.unstable_operations["list_logs"] = True
+with ApiClient(configuration) as api_client:
+    api_instance = LogsApi(api_client)
+    response = api_instance.list_logs(body=body)
+
+    print(response)
