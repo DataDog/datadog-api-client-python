b8c93a671fc01017d916a9571756f848ed33bf83
ci.datadog-api-spec <packages@datadoghq.com>
2022-01-05 18:07:41 +0000

    Regenerate client from commit dc856e9 of spec repo

--- examples/v2/logs/ListLogsGet_2034110533.py
+++ examples/v2/logs/ListLogsGet_2034110533.py
@@ -11,8 +11,8 @@ with ApiClient(configuration) as api_client:
     response = api_instance.list_logs_get(
         filter_query="datadog-agent",
         filter_index="main",
-        filter_from="2020-09-17T11:48:36+01:00",
-        filter_to="2020-09-17T12:48:36+01:00",
+        filter_from="2020-09-17T10:48:36Z",
+        filter_to="2020-09-17T11:48:36Z",
         page_limit=5,
     )
 
