7d8829d380fbde4c1f3a708d8cee4cbf7b00d907
ci.datadog-api-spec <packages@datadoghq.com>
2022-01-18 11:15:45 +0000

    Regenerate client from commit 1d04508 of spec repo

---
+++ examples/v1/dashboards/ListDashboards_1773932563.py
@@ -0,0 +1,13 @@
+"""
+Get deleted dashboards returns "OK" response
+"""
+
+from datadog_api_client.v1 import ApiClient, Configuration
+from datadog_api_client.v1.api.dashboards_api import DashboardsApi
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = DashboardsApi(api_client)
+    response = api_instance.list_dashboards(filter_deleted=True)
+
+    print(response)
