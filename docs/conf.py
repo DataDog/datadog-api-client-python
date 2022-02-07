5ba16941cde45e208da628ee72049e34c5ea99c5
Thomas Hervé <thomas.herve@datadoghq.com>
2022-02-07 12:19:07 +0100

    Cleanup conf

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,3 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -12,27 +6,25 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+
+sys.path.insert(0, os.path.abspath("../src"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Datadog API Client for Python'
-copyright = '2022, Datadog'
-author = 'Datadog'
+project = "Datadog API Client for Python"
+copyright = "2022, Datadog"
+author = "Datadog"
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'm2r2',
-    'sphinx.ext.autodoc'
-]
+extensions = ["m2r2", "sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -45,30 +37,29 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_material'
+html_theme = "sphinx_material"
+
+html_logo = "assets/images/logo.svg"
+html_favicon = "assets/images/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['assets']
+html_static_path = ["assets"]
 
 html_theme_options = {
-
     # Set the name of the project to appear in the navigation.
-    'nav_title': 'Datadog API Client for Python',
-
+    "nav_title": "Datadog API Client for Python",
     # Set the color and the accent color
-    'color_primary': 'deep-purple',
-    'color_accent': 'deep-purple',
-
+    "color_primary": "deep-purple",
+    "color_accent": "deep-purple",
     # Set the repo location to get a badge with stats
-    'repo_url': 'https://github.com/DataDog/datadog-api-client-python/',
-    'repo_name': 'datadog-api-client-python',
-
+    "repo_url": "https://github.com/DataDog/datadog-api-client-python/",
+    "repo_name": "datadog-api-client-python",
     # Visible levels of the global TOC; -1 means unlimited
-    'globaltoc_depth': 3,
+    "globaltoc_depth": 3,
     # If False, expand all TOC entries
-    'globaltoc_collapse': False,
+    "globaltoc_collapse": False,
     # If True, show hidden TOC entries
-    'globaltoc_includehidden': False,
+    "globaltoc_includehidden": False,
 }
