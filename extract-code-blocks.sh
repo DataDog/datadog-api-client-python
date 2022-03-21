933e4ea0f27646771756a6ced53cf1107452dab5
Jiri Kuncar <jiri.kuncar@gmail.com>
2022-03-21 15:27:12 +0100

    Remove legacy examples

--- extract-code-blocks.sh
+++ extract-code-blocks.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-OUTPUT=${1:-examples}
-VERSIONS=${2:-v1,v2}
-
-cd ${0%/*}
-
-VERSIONS=(${VERSIONS//,/ })
-
-for version in "${VERSIONS[@]}"; do
-    cp -Rn examples/generated/${version} ${OUTPUT}/
-done
+echo "[DEPRECATED] Do not use $0"
