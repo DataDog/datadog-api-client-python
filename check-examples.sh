cdbaef51b3adae9b973a5ae4367bee323400e66a
Jiri Kuncar <jiri.kuncar@gmail.com>
2022-03-21 18:14:25 +0100

    check examples

--- check-examples.sh
+++ check-examples.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-./extract-code-blocks.sh examples
-
 ls examples/*/*/*.py | xargs -P $(($(nproc)*2)) -n 1 python -m py_compile
 if [ $? -ne 0 ]; then
     echo -e "Failed to build examples"
