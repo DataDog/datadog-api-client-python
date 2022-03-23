4be38d5cc001935d4ed894da560169fe080a40fb
ci.datadog-api-spec <packages@datadoghq.com>
2022-03-23 09:28:09 +0000

    Regenerate client from commit c00e855e of spec repo

--- examples/v1/synthetics/CreateSyntheticsBrowserTest.py
+++ examples/v1/synthetics/CreateSyntheticsBrowserTest.py
@@ -12,7 +12,6 @@ from datadog_api_client.v1.model.synthetics_config_variable import SyntheticsCon
 from datadog_api_client.v1.model.synthetics_config_variable_type import SyntheticsConfigVariableType
 from datadog_api_client.v1.model.synthetics_device_id import SyntheticsDeviceID
 from datadog_api_client.v1.model.synthetics_step import SyntheticsStep
-from datadog_api_client.v1.model.synthetics_step_params import SyntheticsStepParams
 from datadog_api_client.v1.model.synthetics_step_type import SyntheticsStepType
 from datadog_api_client.v1.model.synthetics_test_options import SyntheticsTestOptions
 from datadog_api_client.v1.model.synthetics_test_options_retry import SyntheticsTestOptionsRetry
@@ -54,7 +53,7 @@ body = SyntheticsBrowserTest(
             allow_failure=False,
             is_critical=True,
             name="Refresh page",
-            params=SyntheticsStepParams(),
+            params=dict(),
             type=SyntheticsStepType("refresh"),
         )
     ],
