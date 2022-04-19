01ab952d2d3c9d90a7544559143dc1c60cbaf2f4
ci.datadog-api-spec <packages@datadoghq.com>
2022-04-19 12:43:38 +0000

    Regenerate client from commit 3906f53a of spec repo

--- src/datadog_api_client/v1/model/event.py
+++ src/datadog_api_client/v1/model/event.py
@@ -75,7 +75,7 @@ class Event(ModelNormal):
         :type alert_type: EventAlertType, optional
 
         :param date_happened: POSIX timestamp of the event. Must be sent as an integer (that is no quotes).
-            Limited to events no older than 18 hours.
+            Limited to events no older than 7 days.
         :type date_happened: int, optional
 
         :param device_name: A device name.
