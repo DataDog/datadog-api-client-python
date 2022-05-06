0e9b8f6311c8974dfc51f886bf1e383eab293a45
ci.datadog-api-spec <packages@datadoghq.com>
2022-05-06 08:37:00 +0000

    Regenerate client from commit 2c917de9 of spec repo

--- examples/v1/downtimes/UpdateDowntime.py
+++ examples/v1/downtimes/UpdateDowntime.py
@@ -12,6 +12,7 @@ DOWNTIME_ID = environ["DOWNTIME_ID"]
 
 body = Downtime(
     message="Example-Update_a_downtime_returns_OK_response-updated",
+    mute_first_recovery_notification=True,
 )
 
 configuration = Configuration()
