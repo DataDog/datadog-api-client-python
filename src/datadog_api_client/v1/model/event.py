875129d1a7662d4f5a09fbc65406de141b83f503
ci.datadog-api-spec <packages@datadoghq.com>
2022-05-09 12:31:48 +0000

    Regenerate client from commit ef28653b of spec repo

--- src/datadog_api_client/v1/model/event.py
+++ src/datadog_api_client/v1/model/event.py
@@ -99,7 +99,7 @@ class Event(ModelNormal):
         :type priority: EventPriority, none_type, optional
 
         :param source_type_name: The type of event being posted. Option examples include nagios, hudson, jenkins, my_apps, chef, puppet, git, bitbucket, etc.
-            A complete list of source attribute values [available here](https://docs.datadoghq.com/integrations/faq/list-of-api-source-attribute-value).
+            The list of standard source attribute values [available here](https://docs.datadoghq.com/integrations/faq/list-of-api-source-attribute-value).
         :type source_type_name: str, optional
 
         :param tags: A list of tags to apply to the event.
