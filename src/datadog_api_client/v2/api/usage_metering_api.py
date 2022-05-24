a082db174e29de16490a3cbd0b729a6261a2ad26
ci.datadog-api-spec <packages@datadoghq.com>
2022-05-24 09:16:58 +0000

    Regenerate client from commit 99066a28 of spec repo

--- src/datadog_api_client/v2/api/usage_metering_api.py
+++ src/datadog_api_client/v2/api/usage_metering_api.py
@@ -144,7 +144,7 @@ class UsageMeteringApi:
     def get_cost_by_org(self, start_month, **kwargs):
         """Get cost across multi-org account.
 
-        Get cost across multi-org account.
+        Get cost across multi-org account. Cost by org data for a given month becomes available no later than the 16th of the following month.
 
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True.
