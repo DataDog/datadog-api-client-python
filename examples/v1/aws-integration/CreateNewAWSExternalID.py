6d6da6bc8e54af4e0379a6d26663941c575bc4a7
Thomas Hervé <thomas.herve@datadoghq.com>
2022-05-25 10:06:33 +0200

    Try again

---
+++ examples/v1/aws-integration/CreateNewAWSExternalID.py
@@ -0,0 +1,36 @@
+"""
+Generate a new external ID returns "OK" response
+"""
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v1.api.aws_integration_api import AWSIntegrationApi
+from datadog_api_client.v1.model.aws_account import AWSAccount
+
+body = AWSAccount(
+    account_id="1234567",
+    account_specific_namespace_rules=dict(
+        auto_scaling=False,
+        opswork=False,
+    ),
+    cspm_resource_collection_enabled=True,
+    excluded_regions=[
+        "us-east-1",
+        "us-west-2",
+    ],
+    filter_tags=[
+        "$KEY:$VALUE",
+    ],
+    host_tags=[
+        "$KEY:$VALUE",
+    ],
+    metrics_collection_enabled=False,
+    resource_collection_enabled=True,
+    role_name="DatadogAWSIntegrationRole",
+)
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = AWSIntegrationApi(api_client)
+    response = api_instance.create_new_aws_external_id(body=body)
+
+    print(response)
---
+++ examples/v2/incidents/UpdateIncident.py
@@ -0,0 +1,43 @@
+"""
+Update an existing incident returns "OK" response
+"""
+
+from os import environ
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v2.api.incidents_api import IncidentsApi
+from datadog_api_client.v2.model.incident_field_attributes_single_value import IncidentFieldAttributesSingleValue
+from datadog_api_client.v2.model.incident_field_attributes_single_value_type import (
+    IncidentFieldAttributesSingleValueType,
+)
+from datadog_api_client.v2.model.incident_type import IncidentType
+from datadog_api_client.v2.model.incident_update_attributes import IncidentUpdateAttributes
+from datadog_api_client.v2.model.incident_update_data import IncidentUpdateData
+from datadog_api_client.v2.model.incident_update_request import IncidentUpdateRequest
+
+# there is a valid "incident" in the system
+INCIDENT_DATA_ATTRIBUTES_TITLE = environ["INCIDENT_DATA_ATTRIBUTES_TITLE"]
+INCIDENT_DATA_ID = environ["INCIDENT_DATA_ID"]
+
+body = IncidentUpdateRequest(
+    data=IncidentUpdateData(
+        id=INCIDENT_DATA_ID,
+        type=IncidentType("incidents"),
+        attributes=IncidentUpdateAttributes(
+            fields=dict(
+                state=IncidentFieldAttributesSingleValue(
+                    type=IncidentFieldAttributesSingleValueType("dropdown"),
+                    value="resolved",
+                ),
+            ),
+            title="A test incident title-updated",
+        ),
+    ),
+)
+
+configuration = Configuration()
+configuration.unstable_operations["update_incident"] = True
+with ApiClient(configuration) as api_client:
+    api_instance = IncidentsApi(api_client)
+    response = api_instance.update_incident(incident_id=INCIDENT_DATA_ID, body=body)
+
+    print(response)
