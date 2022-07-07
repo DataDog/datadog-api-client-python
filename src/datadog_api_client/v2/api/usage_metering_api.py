b9ca18a56c7098b3b220a302ed485363f1a57647
ci.datadog-api-spec <packages@datadoghq.com>
2022-07-07 20:18:05 +0000

    Regenerate client from commit 3b40e043 of spec repo

--- src/datadog_api_client/v2/api/usage_metering_api.py
+++ src/datadog_api_client/v2/api/usage_metering_api.py
@@ -68,45 +68,6 @@ class UsageMeteringApi:
             api_client=api_client,
         )
 
-        self._get_estimated_cost_by_org_endpoint = _Endpoint(
-            settings={
-                "response_type": (CostByOrgResponse,),
-                "auth": ["apiKeyAuth", "appKeyAuth", "AuthZ"],
-                "endpoint_path": "/api/v2/usage/estimated_cost_by_org",
-                "operation_id": "get_estimated_cost_by_org",
-                "http_method": "GET",
-                "version": "v2",
-                "servers": None,
-            },
-            params_map={
-                "start_month": {
-                    "openapi_types": (datetime,),
-                    "attribute": "start_month",
-                    "location": "query",
-                },
-                "end_month": {
-                    "openapi_types": (datetime,),
-                    "attribute": "end_month",
-                    "location": "query",
-                },
-                "start_date": {
-                    "openapi_types": (datetime,),
-                    "attribute": "start_date",
-                    "location": "query",
-                },
-                "end_date": {
-                    "openapi_types": (datetime,),
-                    "attribute": "end_date",
-                    "location": "query",
-                },
-            },
-            headers_map={
-                "accept": ["application/json;datetime-format=rfc3339"],
-                "content_type": [],
-            },
-            api_client=api_client,
-        )
-
         self._get_usage_application_security_monitoring_endpoint = _Endpoint(
             settings={
                 "response_type": (UsageApplicationSecurityMonitoringResponse,),
@@ -220,43 +181,6 @@ class UsageMeteringApi:
             kwargs["end_month"] = end_month
 
         return self._get_cost_by_org_endpoint.call_with_http_info(**kwargs)
-
-    def get_estimated_cost_by_org(
-        self,
-        *,
-        start_month: Union[datetime, UnsetType] = unset,
-        end_month: Union[datetime, UnsetType] = unset,
-        start_date: Union[datetime, UnsetType] = unset,
-        end_date: Union[datetime, UnsetType] = unset,
-    ) -> CostByOrgResponse:
-        """Get estimated cost across multi-org account.
-
-        Get estimated cost across multi-org account.
-
-        :param start_month: Datetime in ISO-8601 format, UTC, precise to month: ``[YYYY-MM]`` for cost beginning this month. Either start_month or start_date should be specified, but not both.
-        :type start_month: datetime, optional
-        :param end_month: Datetime in ISO-8601 format, UTC, precise to month: ``[YYYY-MM]`` for cost ending this month.
-        :type end_month: datetime, optional
-        :param start_date: Datetime in ISO-8601 format, UTC, precise to day: ``[YYYY-MM-DD]`` for cost beginning this day. Either start_month or start_date should be specified, but not both.
-        :type start_date: datetime, optional
-        :param end_date: Datetime in ISO-8601 format, UTC, precise to day: ``[YYYY-MM-DD]`` for cost ending this day.
-        :type end_date: datetime, optional
-        :rtype: CostByOrgResponse
-        """
-        kwargs: Dict[str, Any] = {}
-        if start_month is not unset:
-            kwargs["start_month"] = start_month
-
-        if end_month is not unset:
-            kwargs["end_month"] = end_month
-
-        if start_date is not unset:
-            kwargs["start_date"] = start_date
-
-        if end_date is not unset:
-            kwargs["end_date"] = end_date
-
-        return self._get_estimated_cost_by_org_endpoint.call_with_http_info(**kwargs)
 
     def get_usage_application_security_monitoring(
         self,
