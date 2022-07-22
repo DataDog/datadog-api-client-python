a699be1409cfca9a675590c7b651bb2237fec80c
ci.datadog-api-spec <packages@datadoghq.com>
2022-07-22 18:56:29 +0000

    Regenerate client from commit 692b2bd1 of spec repo

--- examples/v1/usage-metering/GetUsageLogs.py
+++ examples/v1/usage-metering/GetUsageLogs.py
@@ -1,18 +1,17 @@
 """
-Get hourly usage for Logs returns "OK" response
+Get hourly usage for logs returns "OK" response
 """
 
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v1.api.usage_metering_api import UsageMeteringApi
+from datetime import datetime
+from dateutil.tz import tzutc
 
 configuration = Configuration()
 with ApiClient(configuration) as api_client:
     api_instance = UsageMeteringApi(api_client)
     response = api_instance.get_usage_logs(
-        start_hr=(datetime.now() + relativedelta(days=-5)),
-        end_hr=(datetime.now() + relativedelta(days=-3)),
+        start_hr=datetime(2021, 11, 11, 11, 11, 11, 111000, tzinfo=tzutc()),
     )
 
     print(response)
