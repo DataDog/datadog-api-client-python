330c06617f5cfedc76cb29563e617a2839f7cf58
ci.datadog-api-spec <packages@datadoghq.com>
2022-08-09 11:12:23 +0000

    pre-commit fixes

--- src/datadog_api_client/v2/model/rum_application_create_type.py
+++ src/datadog_api_client/v2/model/rum_application_create_type.py
@@ -10,6 +10,12 @@ from datadog_api_client.model_utils import (
 
 
 class RUMApplicationCreateType(ModelSimple):
+    """
+    RUM application creation type.
+
+    :param value: If omitted defaults to "rum_application_create". Must be one of ["rum_application_create"].
+    :type value: str
+    """
 
     allowed_values = {
         "value": {
@@ -22,33 +28,3 @@ class RUMApplicationCreateType(ModelSimple):
         return {
             "value": (str,),
         }
-
-    def __init__(self, *args, **kwargs):
-        """
-        RUM application creation type.
-
-        Note that value can be passed either in args or in kwargs, but not in both.
-
-        :param value: If omitted defaults to "rum_application_create". Must be one of ["rum_application_create"].
-        :type value: str
-        """
-        super().__init__(kwargs)
-
-        if "value" in kwargs:
-            value = kwargs.pop("value")
-        elif args:
-            args = list(args)
-            value = args.pop(0)
-        else:
-            value = "rum_application_create"
-
-        self._check_pos_args(args)
-
-        self.value = value
-
-        self._check_kw_args(kwargs)
-
-    @classmethod
-    def _from_openapi_data(cls, *args, **kwargs):
-        """Helper creating a new instance from a response."""
-        return cls(*args, **kwargs)
