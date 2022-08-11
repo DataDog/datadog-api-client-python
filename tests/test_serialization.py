8068ceea07293405b90329f5c40c0e4514b95309
Thomas Hervé <thomas.herve@datadoghq.com>
2022-08-11 15:00:13 +0200

    Fix oneOf primitive objects creation (#1125)
    We were basing the creation on the presence of optional parameters, so
    it failed and got an unparsed object.

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,6 +1,29 @@
 from datadog_api_client.v1.model.monitor_thresholds import MonitorThresholds
+from datadog_api_client.v2.model.logs_group_by import LogsGroupBy
+from datadog_api_client.v2.model.logs_group_by_missing import LogsGroupByMissing
+from datadog_api_client.v2.model.logs_group_by_total import LogsGroupByTotal
+from datadog_api_client.v2.model.logs_aggregate_sort import LogsAggregateSort
+from datadog_api_client.v2.model.logs_aggregate_sort_type import LogsAggregateSortType
+from datadog_api_client.v2.model.logs_aggregation_function import LogsAggregationFunction
+from datadog_api_client.v2.model.logs_sort_order import LogsSortOrder
 
 
 def test_default_coerce():
     thresholds = MonitorThresholds(critical=5)
     assert thresholds["critical"] == 5.0
+
+
+def test_primitive_one_of():
+    group_by = LogsGroupBy(
+        facet="host",
+        missing=LogsGroupByMissing("miss"),
+        sort=LogsAggregateSort(
+            type=LogsAggregateSortType("measure"),
+            order=LogsSortOrder("asc"),
+            aggregation=LogsAggregationFunction("pc90"),
+            metric="@duration",
+        ),
+        total=LogsGroupByTotal("recall"),
+    )
+
+    assert group_by.missing == "miss"
