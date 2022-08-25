5779fc86b283b77514a0e7a837cb89c9af6bb590
ci.datadog-api-spec <packages@datadoghq.com>
2022-08-25 00:23:05 +0000

    Regenerate client from commit 95ec6eee of spec repo

--- src/datadog_api_client/v1/model/on_missing_data_option.py
+++ src/datadog_api_client/v1/model/on_missing_data_option.py
@@ -14,7 +14,7 @@ class OnMissingDataOption(ModelSimple):
     Controls how groups or monitors are treated if an evaluation does not return any data points.
         The default option results in different behavior depending on the monitor query type.
         For monitors using Count queries, an empty monitor evaluation is treated as 0 and is compared to the threshold conditions.
-        For monitor using any query type other than Count, for example Gauge or Rate, the monitor shows the last known status.
+        For monitors using any query type other than Count, for example Gauge, Measure, or Rate, the monitor shows the last known status.
         This option is only available for APM Trace Analytics, Audit Trail, CI, Error Tracking, Event, Logs, and RUM monitors.
 
     :param value: Must be one of ["default", "show_no_data", "show_and_notify_no_data", "resolve"].
