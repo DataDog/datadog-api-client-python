c686d9312b45daa50fa0fa160f15416137b3c62f
ci.datadog-api-spec <packages@datadoghq.com>
2022-08-29 14:57:38 +0000

    Regenerate client from commit 71afb1ee of spec repo

--- src/datadog_api_client/v1/model/usage_attribution_sort.py
+++ src/datadog_api_client/v1/model/usage_attribution_sort.py
@@ -13,7 +13,7 @@ class UsageAttributionSort(ModelSimple):
     """
     The field to sort by.
 
-    :param value: If omitted defaults to "custom_timeseries_usage". Must be one of ["api_percentage", "snmp_usage", "apm_host_usage", "api_usage", "appsec_usage", "appsec_percentage", "container_usage", "custom_timeseries_percentage", "container_percentage", "apm_host_percentage", "npm_host_percentage", "browser_percentage", "browser_usage", "infra_host_percentage", "snmp_percentage", "npm_host_usage", "infra_host_usage", "custom_timeseries_usage", "lambda_functions_usage", "lambda_functions_percentage", "lambda_invocations_usage", "lambda_invocations_percentage", "estimated_indexed_logs_usage", "estimated_indexed_logs_percentage", "estimated_indexed_spans_usage", "estimated_indexed_spans_percentage", "estimated_ingested_spans_usage", "estimated_ingested_spans_percentage"].
+    :param value: If omitted defaults to "custom_timeseries_usage". Must be one of ["api_percentage", "snmp_usage", "apm_host_usage", "api_usage", "appsec_usage", "appsec_percentage", "container_usage", "custom_timeseries_percentage", "container_percentage", "apm_host_percentage", "npm_host_percentage", "browser_percentage", "browser_usage", "infra_host_percentage", "snmp_percentage", "npm_host_usage", "infra_host_usage", "custom_timeseries_usage", "lambda_functions_usage", "lambda_functions_percentage", "lambda_invocations_usage", "lambda_invocations_percentage", "estimated_indexed_logs_usage", "estimated_indexed_logs_percentage", "estimated_ingested_logs_usage", "estimated_ingested_logs_percentage", "estimated_indexed_spans_usage", "estimated_indexed_spans_percentage", "estimated_ingested_spans_usage", "estimated_ingested_spans_percentage"].
     :type value: str
     """
 
@@ -43,6 +43,8 @@ class UsageAttributionSort(ModelSimple):
             "LAMBDA_INVOCATIONS_PERCENTAGE": "lambda_invocations_percentage",
             "ESTIMATED_INDEXED_LOGS_USAGE": "estimated_indexed_logs_usage",
             "ESTIMATED_INDEXED_LOGS_PERCENTAGE": "estimated_indexed_logs_percentage",
+            "ESTIMATED_INGESTED_LOGS_USAGE": "estimated_ingested_logs_usage",
+            "ESTIMATED_INGESTED_LOGS_PERCENTAGE": "estimated_ingested_logs_percentage",
             "ESTIMATED_INDEXED_SPANS_USAGE": "estimated_indexed_spans_usage",
             "ESTIMATED_INDEXED_SPANS_PERCENTAGE": "estimated_indexed_spans_percentage",
             "ESTIMATED_INGESTED_SPANS_USAGE": "estimated_ingested_spans_usage",
