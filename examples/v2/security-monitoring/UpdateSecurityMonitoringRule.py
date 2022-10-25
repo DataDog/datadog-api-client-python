d5c9726c3042ea3176488f2cd99a471abd8a44a0
ci.datadog-api-spec <packages@datadoghq.com>
2022-10-25 19:33:14 +0000

    Regenerate client from commit 904df7aa of spec repo

--- examples/v2/security-monitoring/UpdateSecurityMonitoringRule.py
+++ examples/v2/security-monitoring/UpdateSecurityMonitoringRule.py
@@ -2,37 +2,16 @@
 Update an existing rule returns "OK" response
 """
 
+from os import environ
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v2.api.security_monitoring_api import SecurityMonitoringApi
-from datadog_api_client.v2.model.security_monitoring_filter import SecurityMonitoringFilter
-from datadog_api_client.v2.model.security_monitoring_filter_action import SecurityMonitoringFilterAction
 from datadog_api_client.v2.model.security_monitoring_rule_case import SecurityMonitoringRuleCase
-from datadog_api_client.v2.model.security_monitoring_rule_detection_method import SecurityMonitoringRuleDetectionMethod
 from datadog_api_client.v2.model.security_monitoring_rule_evaluation_window import (
     SecurityMonitoringRuleEvaluationWindow,
-)
-from datadog_api_client.v2.model.security_monitoring_rule_hardcoded_evaluator_type import (
-    SecurityMonitoringRuleHardcodedEvaluatorType,
-)
-from datadog_api_client.v2.model.security_monitoring_rule_impossible_travel_options import (
-    SecurityMonitoringRuleImpossibleTravelOptions,
 )
 from datadog_api_client.v2.model.security_monitoring_rule_keep_alive import SecurityMonitoringRuleKeepAlive
 from datadog_api_client.v2.model.security_monitoring_rule_max_signal_duration import (
     SecurityMonitoringRuleMaxSignalDuration,
-)
-from datadog_api_client.v2.model.security_monitoring_rule_new_value_options import SecurityMonitoringRuleNewValueOptions
-from datadog_api_client.v2.model.security_monitoring_rule_new_value_options_forget_after import (
-    SecurityMonitoringRuleNewValueOptionsForgetAfter,
-)
-from datadog_api_client.v2.model.security_monitoring_rule_new_value_options_learning_duration import (
-    SecurityMonitoringRuleNewValueOptionsLearningDuration,
-)
-from datadog_api_client.v2.model.security_monitoring_rule_new_value_options_learning_method import (
-    SecurityMonitoringRuleNewValueOptionsLearningMethod,
-)
-from datadog_api_client.v2.model.security_monitoring_rule_new_value_options_learning_threshold import (
-    SecurityMonitoringRuleNewValueOptionsLearningThreshold,
 )
 from datadog_api_client.v2.model.security_monitoring_rule_options import SecurityMonitoringRuleOptions
 from datadog_api_client.v2.model.security_monitoring_rule_query_aggregation import (
@@ -42,52 +21,42 @@ from datadog_api_client.v2.model.security_monitoring_rule_severity import Securi
 from datadog_api_client.v2.model.security_monitoring_rule_update_payload import SecurityMonitoringRuleUpdatePayload
 from datadog_api_client.v2.model.security_monitoring_standard_rule_query import SecurityMonitoringStandardRuleQuery
 
+# there is a valid "security_rule" in the system
+SECURITY_RULE_ID = environ["SECURITY_RULE_ID"]
+
 body = SecurityMonitoringRuleUpdatePayload(
-    cases=[
-        SecurityMonitoringRuleCase(
-            notifications=[],
-            status=SecurityMonitoringRuleSeverity.CRITICAL,
-        ),
-    ],
-    filters=[
-        SecurityMonitoringFilter(
-            action=SecurityMonitoringFilterAction.REQUIRE,
-        ),
-    ],
-    has_extended_title=True,
-    options=SecurityMonitoringRuleOptions(
-        decrease_criticality_based_on_env=False,
-        detection_method=SecurityMonitoringRuleDetectionMethod.THRESHOLD,
-        evaluation_window=SecurityMonitoringRuleEvaluationWindow.ZERO_MINUTES,
-        hardcoded_evaluator_type=SecurityMonitoringRuleHardcodedEvaluatorType.LOG4SHELL,
-        impossible_travel_options=SecurityMonitoringRuleImpossibleTravelOptions(
-            baseline_user_locations=True,
-        ),
-        keep_alive=SecurityMonitoringRuleKeepAlive.ZERO_MINUTES,
-        max_signal_duration=SecurityMonitoringRuleMaxSignalDuration.ZERO_MINUTES,
-        new_value_options=SecurityMonitoringRuleNewValueOptions(
-            forget_after=SecurityMonitoringRuleNewValueOptionsForgetAfter.ONE_DAY,
-            learning_duration=SecurityMonitoringRuleNewValueOptionsLearningDuration.ZERO_DAYS,
-            learning_method=SecurityMonitoringRuleNewValueOptionsLearningMethod.DURATION,
-            learning_threshold=SecurityMonitoringRuleNewValueOptionsLearningThreshold.ZERO_OCCURRENCES,
-        ),
-    ),
+    name="Example-Update_an_existing_rule_returns_OK_response-Updated",
     queries=[
         SecurityMonitoringStandardRuleQuery(
+            query="@test:true",
             aggregation=SecurityMonitoringRuleQueryAggregation.COUNT,
-            distinct_fields=[],
             group_by_fields=[],
+            distinct_fields=[],
             metrics=[],
-            query="a > 3",
         ),
     ],
+    filters=[],
+    cases=[
+        SecurityMonitoringRuleCase(
+            name="",
+            status=SecurityMonitoringRuleSeverity.INFO,
+            condition="a > 0",
+            notifications=[],
+        ),
+    ],
+    options=SecurityMonitoringRuleOptions(
+        evaluation_window=SecurityMonitoringRuleEvaluationWindow.FIFTEEN_MINUTES,
+        keep_alive=SecurityMonitoringRuleKeepAlive.ONE_HOUR,
+        max_signal_duration=SecurityMonitoringRuleMaxSignalDuration.ONE_DAY,
+    ),
+    message="Test rule",
     tags=[],
-    version=1,
+    is_enabled=True,
 )
 
 configuration = Configuration()
 with ApiClient(configuration) as api_client:
     api_instance = SecurityMonitoringApi(api_client)
-    response = api_instance.update_security_monitoring_rule(rule_id="rule_id", body=body)
+    response = api_instance.update_security_monitoring_rule(rule_id=SECURITY_RULE_ID, body=body)
 
     print(response)
