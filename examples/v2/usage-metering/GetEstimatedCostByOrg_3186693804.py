ebc0b6cabc24b0012f10dff510373c565eaabe6b
ci.datadog-api-spec <packages@datadoghq.com>
2022-11-04 18:43:59 +0000

    Regenerate client from commit af3f11fc of spec repo

--- examples/v2/usage-metering/GetEstimatedCostByOrg_3186693804.py
+++ examples/v2/usage-metering/GetEstimatedCostByOrg_3186693804.py
@@ -3,7 +3,6 @@ GetEstimatedCostByOrg with start_month returns "OK" response
 """
 
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v2.api.usage_metering_api import UsageMeteringApi
 
@@ -12,7 +11,7 @@ with ApiClient(configuration) as api_client:
     api_instance = UsageMeteringApi(api_client)
     response = api_instance.get_estimated_cost_by_org(
         view="sub-org",
-        start_month=(datetime.now() + relativedelta(days=-5)),
+        start_month=datetime.now(),
     )
 
     print(response)
