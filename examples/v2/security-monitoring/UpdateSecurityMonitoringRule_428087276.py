42365532cd8fd7e0f43781ce5db1e6620449264e
ci.datadog-api-spec <packages@datadoghq.com>
2022-12-19 10:29:46 +0000

    Regenerate client from commit 45a3d9c9 of spec repo

--- examples/v2/security-monitoring/UpdateSecurityMonitoringRule_428087276.py
+++ examples/v2/security-monitoring/UpdateSecurityMonitoringRule_428087276.py
@@ -31,6 +31,7 @@ body = SecurityMonitoringRuleUpdatePayload(
     ],
     options=SecurityMonitoringRuleOptions(
         compliance_rule_options=CloudConfigurationComplianceRuleOptions(
+            resource_type="gcp_compute_disk",
             rego_rule=CloudConfigurationRegoRule(
                 policy="package datadog\n",
                 resource_types=[
