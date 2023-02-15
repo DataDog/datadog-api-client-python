6da8302f2d2121c99449c525dc5c997da169211c
ci.datadog-api-spec <packages@datadoghq.com>
2023-02-15 14:55:06 +0000

    Regenerate client from commit 3371aaa2 of spec repo

--- src/datadog_api_client/v1/model/slo_list_widget_query.py
+++ src/datadog_api_client/v1/model/slo_list_widget_query.py
@@ -3,7 +3,7 @@
 # Copyright 2019-Present Datadog, Inc.
 from __future__ import annotations
 
-from typing import Union
+from typing import List, Union, TYPE_CHECKING
 
 from datadog_api_client.model_utils import (
     ModelNormal,
@@ -11,6 +11,10 @@ from datadog_api_client.model_utils import (
     unset,
     UnsetType,
 )
+
+
+if TYPE_CHECKING:
+    from datadog_api_client.v1.model.widget_field_sort import WidgetFieldSort
 
 
 class SLOListWidgetQuery(ModelNormal):
@@ -23,17 +27,27 @@ class SLOListWidgetQuery(ModelNormal):
 
     @cached_property
     def openapi_types(_):
+        from datadog_api_client.v1.model.widget_field_sort import WidgetFieldSort
+
         return {
             "limit": (int,),
             "query_string": (str,),
+            "sort": ([WidgetFieldSort],),
         }
 
     attribute_map = {
         "limit": "limit",
         "query_string": "query_string",
+        "sort": "sort",
     }
 
-    def __init__(self_, query_string: str, limit: Union[int, UnsetType] = unset, **kwargs):
+    def __init__(
+        self_,
+        query_string: str,
+        limit: Union[int, UnsetType] = unset,
+        sort: Union[List[WidgetFieldSort], UnsetType] = unset,
+        **kwargs,
+    ):
         """
         Updated SLO List widget.
 
@@ -42,9 +56,14 @@ class SLOListWidgetQuery(ModelNormal):
 
         :param query_string: Widget query.
         :type query_string: str
+
+        :param sort: Options for sorting results.
+        :type sort: [WidgetFieldSort], optional
         """
         if limit is not unset:
             kwargs["limit"] = limit
+        if sort is not unset:
+            kwargs["sort"] = sort
         super().__init__(kwargs)
 
         self_.query_string = query_string
