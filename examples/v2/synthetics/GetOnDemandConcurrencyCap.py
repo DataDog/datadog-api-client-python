c327ea24cf397a7006ef78cc105f09dac73e2f59
ci.datadog-api-spec <packages@datadoghq.com>
2023-02-17 16:20:08 +0000

    Regenerate client from commit a05a9d36 of spec repo

---
+++ examples/v2/synthetics/GetOnDemandConcurrencyCap.py
@@ -0,0 +1,13 @@
+"""
+Get the on-demand concurrency cap returns "OK" response
+"""
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v2.api.synthetics_api import SyntheticsApi
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = SyntheticsApi(api_client)
+    response = api_instance.get_on_demand_concurrency_cap()
+
+    print(response)
