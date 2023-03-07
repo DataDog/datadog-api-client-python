467b3baafbef1138f1c7dd8a8454be7c6d2e1c1e
ci.datadog-api-spec <packages@datadoghq.com>
2023-03-07 18:55:20 +0000

    Regenerate client from commit f09bf63c of spec repo

--- src/datadog_api_client/v2/api/restriction_policies_api.py
+++ src/datadog_api_client/v2/api/restriction_policies_api.py
@@ -108,7 +108,7 @@ class RestrictionPoliciesApi:
 
         Deletes the restriction policy associated with a specified resource.
 
-        :param resource_id: Identifier, formatted as ``type:id``. Supported types: ``connection`` , ``dashboard`` , ``notebook`` , ``security-rule``.
+        :param resource_id: Identifier, formatted as ``type:id``. Supported types: ``connection`` , ``dashboard`` , ``notebook`` , ``security-rule`` , ``slo``.
         :type resource_id: str
         :rtype: None
         """
@@ -125,7 +125,7 @@ class RestrictionPoliciesApi:
 
         Retrieves the restriction policy associated with a specified resource.
 
-        :param resource_id: Identifier, formatted as ``type:id``. Supported types: ``connection`` , ``dashboard`` , ``notebook`` , ``security-rule``.
+        :param resource_id: Identifier, formatted as ``type:id``. Supported types: ``connection`` , ``dashboard`` , ``notebook`` , ``security-rule`` , ``slo``.
         :type resource_id: str
         :rtype: RestrictionPolicyResponse
         """
@@ -151,8 +151,9 @@ class RestrictionPoliciesApi:
         * Dashboards: ``dashboard``
         * Notebooks: ``notebook``
         * Security Rules: ``security-rule``
+        * Service Level Objectives: ``slo``
 
-        :param resource_id: Identifier, formatted as ``type:id``. Supported types: ``connection`` , ``dashboard`` , ``notebook`` , ``security-rule``.
+        :param resource_id: Identifier, formatted as ``type:id``. Supported types: ``connection`` , ``dashboard`` , ``notebook`` , ``security-rule`` , ``slo``.
         :type resource_id: str
         :param body: Restriction policy payload
         :type body: RestrictionPolicyUpdateRequest
