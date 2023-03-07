fd2f5b34701b989026e4a527fb264230461059d5
ci.datadog-api-spec <packages@datadoghq.com>
2023-03-07 22:36:59 +0000

    Regenerate client from commit 73e66c9a of spec repo

--- src/datadog_api_client/v1/model/cancel_downtimes_by_scope_request.py
+++ src/datadog_api_client/v1/model/cancel_downtimes_by_scope_request.py
@@ -25,7 +25,7 @@ class CancelDowntimesByScopeRequest(ModelNormal):
         """
         Cancel downtimes according to scope.
 
-        :param scope: The scope(s) to which the downtime applies. For example, ``host:app2``.
+        :param scope: The scope(s) to which the downtime applies and must be in ``key:value`` format. For example, ``host:app2``.
             Provide multiple scopes as a comma-separated list like ``env:dev,env:prod``.
             The resulting downtime applies to sources that matches ALL provided scopes ( ``env:dev`` **AND** ``env:prod`` ).
         :type scope: str
