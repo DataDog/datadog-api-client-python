40e30f31857bddfbab0b193f500a1746ee33c567
ci.datadog-api-spec <packages@datadoghq.com>
2023-04-14 08:33:59 +0000

    Regenerate client from commit 318954bb of spec repo

--- examples/v2/logs/SubmitLog.py
+++ examples/v2/logs/SubmitLog.py
@@ -15,7 +15,7 @@ body = HTTPLog(
             hostname="i-012345678",
             message="2019-11-19T14:37:58,995 INFO [process.name][20081] Hello World",
             service="payment",
-            status="error",
+            status="info",
         ),
     ]
 )
