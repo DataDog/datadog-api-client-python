3483545802295952733302cae646283f6b9fe6fb
skarimo <40482491+skarimo@users.noreply.github.com>
2023-06-07 12:48:08 -0400

    fix example generation for oneOf (#1512)

--- .generator/src/generator/formatter.py
+++ .generator/src/generator/formatter.py
@@ -350,6 +350,11 @@ def format_data_with_schema_dict(
 
     parameters = ""
     if "properties" in schema:
+        required_properties = set(schema.get("required", []))
+        missing = required_properties - set(data.keys())
+        if missing:
+            raise ValueError(f"missing required properties: {missing}")
+
         for k, v in data.items():
             if k in schema["properties"]:
                 sub_schema = schema["properties"][k]
