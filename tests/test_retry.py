1f4fdc515d249c3a37d49f804afa3f928d944763
HantingZhang2 <hanting.zhang@datadoghq.com>
2023-07-18 17:35:25 +0200

    Fixe test & adjusting retry impl

--- tests/test_retry.py
+++ tests/test_retry.py
@@ -1,19 +1,19 @@
-import os
 import pytest
+from unittest import mock
 
 from datadog_api_client.api_client import ApiClient
 from datadog_api_client.configuration import Configuration
-from datadog_api_client.exceptions import ApiException
-from datadog_api_client.v1.api import metrics_api
-
+from datadog_api_client.v2.api.logs_api import LogsApi
+from datadog_api_client.api_client import ApiClient
+from datadog_api_client.configuration import Configuration
 
 @pytest.mark.vcr(ignore_localhost=True)
-@pytest.mark.skipif(os.getenv("RECORD", "false").lower() != "false", reason="random")
-def test_retry():
-    configuration = Configuration()
+@mock.patch("datadog_api_client.rest.DDRetry.get_retry_after")
+def test_retry_get_list_logs(retry_mock):
+    retry_mock.return_value = 0.1
+    configuration= Configuration()
     configuration.enable_retry=True
-    configuration.debug=True
     with ApiClient(configuration) as api_client:
-        api_instance = metrics_api.MetricsApi(api_client)
-        with pytest.raises(ApiException):
-            api_instance.get_metric_metadata("some_metric")
+        api_instance = LogsApi(api_client)
+        response = api_instance.list_logs_get()
+    assert retry_mock.call_count == 3 and response!=None
