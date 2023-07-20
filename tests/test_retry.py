23d9f0788c76260d366df44f25e36d0ffe04927c
ci.datadog-api-spec <packages@datadoghq.com>
2023-07-20 16:28:10 +0000

    pre-commit fixes

--- tests/test_retry.py
+++ tests/test_retry.py
@@ -5,15 +5,15 @@ from datadog_api_client.rest import DDRetry
 
 
 @mock.patch("urllib3.connectionpool.HTTPConnectionPool._get_conn")
-#@mock.patch("urllib3.PoolManager.request")
+# @mock.patch("urllib3.PoolManager.request")
 def test_retry_request_ddretry(getconn_mock):
     ddretries = DDRetry(total=3)
     http = urllib3.PoolManager(retries=ddretries)
     mock_endpoint = "/api/test"
 
-    #mock_header= "{"headers":{"x-ratelimit-reset":["1"]}}"
-    mock_msg=HTTPMessage()
-    mock_msg.add_header("X-Ratelimit-Reset","124")
+    # mock_header= "{"headers":{"x-ratelimit-reset":["1"]}}"
+    mock_msg = HTTPMessage()
+    mock_msg.add_header("X-Ratelimit-Reset", "124")
     getconn_mock.return_value.getresponse.side_effect = [
         mock.Mock(status=429, msg=mock_msg),
         mock.Mock(status=429, msg=mock_msg),
@@ -21,7 +21,7 @@ def test_retry_request_ddretry(getconn_mock):
     ]
 
     http.request("GET", "http://ddog.url" + mock_endpoint)
-    
+
     assert getconn_mock.return_value.request.mock_calls == [
         mock.call("GET", mock_endpoint, body=None, headers=mock.ANY),
         mock.call("GET", mock_endpoint, body=None, headers=mock.ANY),
