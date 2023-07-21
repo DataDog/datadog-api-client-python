2a53f89354532f2aec5b2ec7b4c975ad8b850eae
HantingZhang2 <hanting.zhang@datadoghq.com>
2023-07-21 12:31:30 +0200

    update test_retry

--- tests/test_retry.py
+++ tests/test_retry.py
@@ -1,27 +1,18 @@
 from unittest import mock
 import urllib3
+import http
 from datadog_api_client.rest import DDRetry
-
 
 @mock.patch("urllib3.connectionpool.HTTPConnectionPool._get_conn")
 def test_retry_request_ddretry(getconn_mock):
     ddretries = DDRetry(total=3)
     pool_manager = urllib3.PoolManager(retries=ddretries)
     mock_endpoint = "/api/test"
-
-    response_429 = urllib3.HTTPResponse(
-        headers={"X-Ratelimit-Reset": "1"},
-        status=429,
-        reason="Too many requests",
-        request_method="GET",
-    )
-
-    response_200 = urllib3.HTTPResponse(
-        headers={"X-Ratelimit-Reset": "1"},
-        status=200,
-        reason="OK",
-        request_method="GET",
-    )
+    msg=http.client.HTTPMessage()
+    response_429 = mock.Mock(status=429, msg=msg, headers={"X-Ratelimit-Reset": "1"})
+    response_429.get_redirect_location.return_value = ""
+    response_200 = mock.Mock(status=200, msg=msg, headers={})
+    response_200.get_redirect_location.return_value = ""
 
     getconn_mock.return_value.getresponse.side_effect = [
         response_429,
@@ -29,37 +20,9 @@ def test_retry_request_ddretry(getconn_mock):
         response_200,
     ]
 
-    pool_manager.request("GET", "http://ddog.url" + mock_endpoint)
-
+    pool_manager.request("GET", "http://ddog.url"+mock_endpoint)
     assert getconn_mock.return_value.request.mock_calls == [
-        mock.call(
-            "GET",
-            mock_endpoint,
-            body=None,
-            headers=mock.ANY,
-            chunked=False,
-            preload_content=True,
-            decode_content=True,
-            enforce_content_length=True,
-        ),
-        mock.call(
-            "GET",
-            mock_endpoint,
-            body=None,
-            headers=mock.ANY,
-            chunked=False,
-            preload_content=True,
-            decode_content=True,
-            enforce_content_length=True,
-        ),
-        mock.call(
-            "GET",
-            mock_endpoint,
-            body=None,
-            headers=mock.ANY,
-            chunked=False,
-            preload_content=True,
-            decode_content=True,
-            enforce_content_length=True,
-        ),
+        mock.call("GET", mock_endpoint, body=None, headers=mock.ANY, chunked=False, preload_content=True, decode_content=True, enforce_content_length=True),
+        mock.call("GET",mock_endpoint, body=None, headers=mock.ANY, chunked=False, preload_content=True, decode_content=True, enforce_content_length=True),
+        mock.call("GET", mock_endpoint, body=None, headers=mock.ANY, chunked=False, preload_content=True, decode_content=True, enforce_content_length=True),
     ]
