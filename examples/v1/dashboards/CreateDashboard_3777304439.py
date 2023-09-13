be8abe2ee010bb5b011a76fd68e4696284ae23ea
ci.datadog-api-spec <packages@datadoghq.com>
2023-09-13 14:38:43 +0000

    Regenerate client from commit ec46249d of spec repo

---
+++ examples/v1/dashboards/CreateDashboard_3777304439.py
@@ -0,0 +1,74 @@
+"""
+Create a new dashboard with formula and function heatmap widget
+"""
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v1.api.dashboards_api import DashboardsApi
+from datadog_api_client.v1.model.dashboard import Dashboard
+from datadog_api_client.v1.model.dashboard_layout_type import DashboardLayoutType
+from datadog_api_client.v1.model.formula_and_function_metric_data_source import FormulaAndFunctionMetricDataSource
+from datadog_api_client.v1.model.formula_and_function_metric_query_definition import (
+    FormulaAndFunctionMetricQueryDefinition,
+)
+from datadog_api_client.v1.model.formula_and_function_response_format import FormulaAndFunctionResponseFormat
+from datadog_api_client.v1.model.heat_map_widget_definition import HeatMapWidgetDefinition
+from datadog_api_client.v1.model.heat_map_widget_definition_type import HeatMapWidgetDefinitionType
+from datadog_api_client.v1.model.heat_map_widget_request import HeatMapWidgetRequest
+from datadog_api_client.v1.model.widget import Widget
+from datadog_api_client.v1.model.widget_formula import WidgetFormula
+from datadog_api_client.v1.model.widget_layout import WidgetLayout
+from datadog_api_client.v1.model.widget_style import WidgetStyle
+from datadog_api_client.v1.model.widget_text_align import WidgetTextAlign
+from datadog_api_client.v1.model.widget_time import WidgetTime
+
+body = Dashboard(
+    title="Example-Dashboard",
+    widgets=[
+        Widget(
+            layout=WidgetLayout(
+                x=0,
+                y=0,
+                width=47,
+                height=15,
+            ),
+            definition=HeatMapWidgetDefinition(
+                title="",
+                title_size="16",
+                title_align=WidgetTextAlign.LEFT,
+                time=WidgetTime(),
+                type=HeatMapWidgetDefinitionType.HEATMAP,
+                requests=[
+                    HeatMapWidgetRequest(
+                        response_format=FormulaAndFunctionResponseFormat.TIMESERIES,
+                        queries=[
+                            FormulaAndFunctionMetricQueryDefinition(
+                                data_source=FormulaAndFunctionMetricDataSource.METRICS,
+                                name="query1",
+                                query="avg:system.cpu.user{*}",
+                            ),
+                        ],
+                        formulas=[
+                            WidgetFormula(
+                                formula="query1",
+                            ),
+                        ],
+                        style=WidgetStyle(
+                            palette="dog_classic",
+                        ),
+                    ),
+                ],
+            ),
+        ),
+    ],
+    template_variables=[],
+    layout_type=DashboardLayoutType.FREE,
+    is_read_only=False,
+    notify_list=[],
+)
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = DashboardsApi(api_client)
+    response = api_instance.create_dashboard(body=body)
+
+    print(response)
