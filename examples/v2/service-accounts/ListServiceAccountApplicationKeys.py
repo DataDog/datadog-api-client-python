25f4e5e7f6b16196dbf965c6b8d85d414ad8380f
ci.datadog-api-spec <packages@datadoghq.com>
2023-10-13 08:14:50 +0000

    Regenerate client from commit 30a99088 of spec repo

--- examples/v2/service-accounts/ListServiceAccountApplicationKeys.py
+++ examples/v2/service-accounts/ListServiceAccountApplicationKeys.py
@@ -2,14 +2,18 @@
 List application keys for this service account returns "OK" response
 """
 
+from os import environ
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v2.api.service_accounts_api import ServiceAccountsApi
+
+# there is a valid "service_account_user" in the system
+SERVICE_ACCOUNT_USER_DATA_ID = environ["SERVICE_ACCOUNT_USER_DATA_ID"]
 
 configuration = Configuration()
 with ApiClient(configuration) as api_client:
     api_instance = ServiceAccountsApi(api_client)
     response = api_instance.list_service_account_application_keys(
-        service_account_id="00000000-0000-1234-0000-000000000000",
+        service_account_id=SERVICE_ACCOUNT_USER_DATA_ID,
     )
 
     print(response)
