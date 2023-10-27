f6a5dbf1e9ba2bb873aa32fca148645ff1ee35ec
ci.datadog-api-spec <packages@datadoghq.com>
2023-10-27 10:24:02 +0000

    pre-commit fixes

--- src/datadog_api_client/v2/model/service_definition_v2_dot2.py
+++ src/datadog_api_client/v2/model/service_definition_v2_dot2.py
@@ -13,6 +13,7 @@ from datadog_api_client.model_utils import (
     none_type,
     unset,
     UnsetType,
+    UUID,
 )
 
 
@@ -51,6 +52,7 @@ class ServiceDefinitionV2Dot2(ModelNormal):
                         int,
                         list,
                         str,
+                        UUID,
                         none_type,
                     )
                 },
@@ -117,7 +119,7 @@ class ServiceDefinitionV2Dot2(ModelNormal):
         :type description: str, optional
 
         :param extensions: Extensions to v2.2 schema.
-        :type extensions: {str: (bool, date, datetime, dict, float, int, list, str, none_type,)}, optional
+        :type extensions: {str: (bool, date, datetime, dict, float, int, list, str, UUID, none_type,)}, optional
 
         :param integrations: Third party integrations that Datadog supports.
         :type integrations: ServiceDefinitionV2Dot2Integrations, optional
