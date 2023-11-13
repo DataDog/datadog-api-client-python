db3bc5c736fb21cd506962dd6809fcd07d1ecf0d
ci.datadog-api-spec <packages@datadoghq.com>
2023-11-13 15:34:00 +0000

    Regenerate client from commit c261ff98 of spec repo

---
+++ examples/v2/okta-integration/ListOktaAccounts.py
@@ -0,0 +1,13 @@
+"""
+Okta Public - List accounts returns "OK" response
+"""
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v2.api.okta_integration_api import OktaIntegrationApi
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = OktaIntegrationApi(api_client)
+    response = api_instance.list_okta_accounts()
+
+    print(response)
