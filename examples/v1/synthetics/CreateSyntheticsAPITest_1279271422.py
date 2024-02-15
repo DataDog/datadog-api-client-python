921b573383ef136a641bc036ecfa4ed31eecfff2
ci.datadog-api-spec <packages@datadoghq.com>
2024-02-15 11:25:37 +0000

    Regenerate client from commit 14c48bc8 of spec repo

--- examples/v1/synthetics/CreateSyntheticsAPITest_1279271422.py
+++ examples/v1/synthetics/CreateSyntheticsAPITest_1279271422.py
@@ -19,7 +19,9 @@ from datadog_api_client.v1.model.synthetics_global_variable_parse_test_options_t
 )
 from datadog_api_client.v1.model.synthetics_global_variable_parser_type import SyntheticsGlobalVariableParserType
 from datadog_api_client.v1.model.synthetics_parsing_options import SyntheticsParsingOptions
+from datadog_api_client.v1.model.synthetics_test_call_type import SyntheticsTestCallType
 from datadog_api_client.v1.model.synthetics_test_details_sub_type import SyntheticsTestDetailsSubType
+from datadog_api_client.v1.model.synthetics_test_metadata import SyntheticsTestMetadata
 from datadog_api_client.v1.model.synthetics_test_options import SyntheticsTestOptions
 from datadog_api_client.v1.model.synthetics_test_options_retry import SyntheticsTestOptionsRetry
 from datadog_api_client.v1.model.synthetics_test_request import SyntheticsTestRequest
@@ -68,6 +70,34 @@ body = SyntheticsAPITest(
                     interval=1000.0,
                 ),
                 subtype=SyntheticsAPIStepSubtype.HTTP,
+            ),
+            SyntheticsAPIStep(
+                name="GRPC CALL",
+                subtype=SyntheticsAPIStepSubtype.GRPC,
+                extracted_values=[],
+                allow_failure=False,
+                is_critical=True,
+                retry=SyntheticsTestOptionsRetry(
+                    count=0,
+                    interval=300.0,
+                ),
+                assertions=[
+                    SyntheticsAssertionTarget(
+                        operator=SyntheticsAssertionOperator.LESS_THAN,
+                        type=SyntheticsAssertionType.RESPONSE_TIME,
+                        target=1000,
+                    ),
+                ],
+                request=SyntheticsTestRequest(
+                    host="grpcbin.test.k6.io",
+                    port=9000,
+                    service="grpcbin.GRPCBin",
+                    method="Index",
+                    message="{}",
+                    compressed_json_descriptor="eJy1lU1z2yAQhv+Lzj74I3ETH506bQ7OZOSm1w4Wa4epBARQppqM/3v5koCJJdvtxCdW77vPssCO3zMKUgHOFu/ZXvBiS6hZho/f8qe7pftYgXphWJrlA8XwxywEvNba+6PhkC2yVcVVswYp0R6ykRYlZ1SCV21SDrxsssPIeS9FJKqGfK2rqnmmSBwhWa2XlKgtaQPiDcRGCUDVfwGD2sKUqKEtc1cSoOrsMlaMOec1sySYCCgUYRSVLv2zSva2u+FQkB0pVkIw8bFuIudOOn3pOaKYVT3Iy97Pd0AYhOx5QcMsnxvRHlnuLf8ETDd3CNtrv2nejkDpRnANCmGkkFn/hsYzpBKE7jVbufgnKnV9HRM9zRPDDKPttYT61n0TdWkAAjggk9AhuxIeaXd69CYTcsGw7cBTakLVbNpRzGEgyWjkSOpMbZXkhGL6oX30R49qt3GoHrap7i0XdD41WQ+2icCNm5p1hmFqnHNlcla0riKmDZ183crDxChjbnurtxHPRE784sVhWvDfGP+SsTKibU3o5NtWHuZFGZOxP6P5VXqIOvaOSec4eYohyd7NslHuJbd1bewds85xYrNxkr2d+5IhFWF3NvaO684xjE2S5ulY+tu64Pna0fCPJgzw6vF5/WucLcYjt5xoq19O3UDptOg/OamJQRaCcPPnMTQ2QDFn+uhPvUfnCrMc99upyQY4Ui9Dlc/YoG3R/v4Cs9YE+g==",
+                    metadata=SyntheticsTestMetadata(),
+                    call_type=SyntheticsTestCallType.UNARY,
+                ),
             ),
         ],
     ),
