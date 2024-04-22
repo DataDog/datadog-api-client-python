eb2047c9e78aeb0b0e2d158064fd54332415a253
ci.datadog-api-spec <packages@datadoghq.com>
2024-04-22 15:10:00 +0000

    Regenerate client from commit 74f81c2f of spec repo

--- src/datadog_api_client/v1/model/hourly_usage_attribution_usage_type.py
+++ src/datadog_api_client/v1/model/hourly_usage_attribution_usage_type.py
@@ -16,7 +16,7 @@ class HourlyUsageAttributionUsageType(ModelSimple):
     """
     Supported products for hourly usage attribution requests.
 
-    :param value: Must be one of ["api_usage", "apm_fargate_usage", "apm_host_usage", "apm_usm_usage", "appsec_fargate_usage", "appsec_usage", "asm_serverless_traced_invocations_usage", "asm_serverless_traced_invocations_percentage", "browser_usage", "ci_pipeline_indexed_spans_usage", "ci_test_indexed_spans_usage", "ci_visibility_itr_usage", "cloud_siem_usage", "container_excl_agent_usage", "container_usage", "cspm_containers_usage", "cspm_hosts_usage", "custom_event_usage", "custom_ingested_timeseries_usage", "custom_timeseries_usage", "cws_containers_usage", "cws_hosts_usage", "dbm_hosts_usage", "dbm_queries_usage", "error_tracking_usage", "error_tracking_percentage", "estimated_indexed_logs_usage", "estimated_indexed_spans_usage", "estimated_ingested_logs_usage", "estimated_ingested_spans_usage", "estimated_rum_sessions_usage", "fargate_usage", "functions_usage", "incident_management_monthly_active_users_usage", "indexed_spans_usage", "infra_host_usage", "ingested_logs_bytes_usage", "ingested_spans_bytes_usage", "invocations_usage", "lambda_traced_invocations_usage", "logs_indexed_15day_usage", "logs_indexed_180day_usage", "logs_indexed_30day_usage", "logs_indexed_360day_usage", "logs_indexed_3day_usage", "logs_indexed_45day_usage", "logs_indexed_60day_usage", "logs_indexed_7day_usage", "logs_indexed_90day_usage", "logs_indexed_custom_retention_usage", "mobile_app_testing_usage", "ndm_netflow_usage", "npm_host_usage", "obs_pipeline_bytes_usage", "obs_pipelines_vcpu_usage", "online_archive_usage", "profiled_container_usage", "profiled_fargate_usage", "profiled_host_usage", "rum_browser_mobile_sessions_usage", "rum_replay_sessions_usage", "sds_scanned_bytes_usage", "serverless_apps_usage", "siem_ingested_bytes_usage", "snmp_usage", "universal_service_monitoring_usage", "vuln_management_hosts_usage", "workflow_executions_usage"].
+    :param value: Must be one of ["api_usage", "apm_fargate_usage", "apm_host_usage", "apm_usm_usage", "appsec_fargate_usage", "appsec_usage", "asm_serverless_traced_invocations_usage", "asm_serverless_traced_invocations_percentage", "browser_usage", "ci_pipeline_indexed_spans_usage", "ci_test_indexed_spans_usage", "ci_visibility_itr_usage", "cloud_siem_usage", "container_excl_agent_usage", "container_usage", "cspm_containers_usage", "cspm_hosts_usage", "custom_event_usage", "custom_ingested_timeseries_usage", "custom_timeseries_usage", "cws_containers_usage", "cws_hosts_usage", "dbm_hosts_usage", "dbm_queries_usage", "error_tracking_usage", "error_tracking_percentage", "estimated_indexed_logs_usage", "estimated_indexed_spans_usage", "estimated_ingested_logs_usage", "estimated_ingested_spans_usage", "estimated_rum_sessions_usage", "fargate_usage", "functions_usage", "incident_management_monthly_active_users_usage", "indexed_spans_usage", "infra_host_usage", "ingested_logs_bytes_usage", "ingested_spans_bytes_usage", "invocations_usage", "lambda_traced_invocations_usage", "logs_indexed_15day_usage", "logs_indexed_180day_usage", "logs_indexed_1day_usage", "logs_indexed_30day_usage", "logs_indexed_360day_usage", "logs_indexed_3day_usage", "logs_indexed_45day_usage", "logs_indexed_60day_usage", "logs_indexed_7day_usage", "logs_indexed_90day_usage", "logs_indexed_custom_retention_usage", "mobile_app_testing_usage", "ndm_netflow_usage", "npm_host_usage", "obs_pipeline_bytes_usage", "obs_pipelines_vcpu_usage", "online_archive_usage", "profiled_container_usage", "profiled_fargate_usage", "profiled_host_usage", "rum_browser_mobile_sessions_usage", "rum_replay_sessions_usage", "sds_scanned_bytes_usage", "serverless_apps_usage", "siem_ingested_bytes_usage", "snmp_usage", "universal_service_monitoring_usage", "vuln_management_hosts_usage", "workflow_executions_usage"].
     :type value: str
     """
 
@@ -63,6 +63,7 @@ class HourlyUsageAttributionUsageType(ModelSimple):
         "lambda_traced_invocations_usage",
         "logs_indexed_15day_usage",
         "logs_indexed_180day_usage",
+        "logs_indexed_1day_usage",
         "logs_indexed_30day_usage",
         "logs_indexed_360day_usage",
         "logs_indexed_3day_usage",
@@ -132,6 +133,7 @@ class HourlyUsageAttributionUsageType(ModelSimple):
     LAMBDA_TRACED_INVOCATIONS_USAGE: ClassVar["HourlyUsageAttributionUsageType"]
     LOGS_INDEXED_15DAY_USAGE: ClassVar["HourlyUsageAttributionUsageType"]
     LOGS_INDEXED_180DAY_USAGE: ClassVar["HourlyUsageAttributionUsageType"]
+    LOGS_INDEXED_1DAY_USAGE: ClassVar["HourlyUsageAttributionUsageType"]
     LOGS_INDEXED_30DAY_USAGE: ClassVar["HourlyUsageAttributionUsageType"]
     LOGS_INDEXED_360DAY_USAGE: ClassVar["HourlyUsageAttributionUsageType"]
     LOGS_INDEXED_3DAY_USAGE: ClassVar["HourlyUsageAttributionUsageType"]
@@ -236,6 +238,7 @@ HourlyUsageAttributionUsageType.LAMBDA_TRACED_INVOCATIONS_USAGE = HourlyUsageAtt
 )
 HourlyUsageAttributionUsageType.LOGS_INDEXED_15DAY_USAGE = HourlyUsageAttributionUsageType("logs_indexed_15day_usage")
 HourlyUsageAttributionUsageType.LOGS_INDEXED_180DAY_USAGE = HourlyUsageAttributionUsageType("logs_indexed_180day_usage")
+HourlyUsageAttributionUsageType.LOGS_INDEXED_1DAY_USAGE = HourlyUsageAttributionUsageType("logs_indexed_1day_usage")
 HourlyUsageAttributionUsageType.LOGS_INDEXED_30DAY_USAGE = HourlyUsageAttributionUsageType("logs_indexed_30day_usage")
 HourlyUsageAttributionUsageType.LOGS_INDEXED_360DAY_USAGE = HourlyUsageAttributionUsageType("logs_indexed_360day_usage")
 HourlyUsageAttributionUsageType.LOGS_INDEXED_3DAY_USAGE = HourlyUsageAttributionUsageType("logs_indexed_3day_usage")
