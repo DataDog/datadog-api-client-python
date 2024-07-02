92dde966a4e2ee77d2f55c8568d176e8e487a8a6
ci.datadog-api-spec <packages@datadoghq.com>
2024-07-02 18:50:50 +0000

    Regenerate client from commit fae9d797 of spec repo

--- src/datadog_api_client/v1/model/synthetics_delete_tests_payload.py
+++ src/datadog_api_client/v1/model/synthetics_delete_tests_payload.py
@@ -17,21 +17,34 @@ class SyntheticsDeleteTestsPayload(ModelNormal):
     @cached_property
     def openapi_types(_):
         return {
+            "force_delete_dependencies": (bool,),
             "public_ids": ([str],),
         }
 
     attribute_map = {
+        "force_delete_dependencies": "force_delete_dependencies",
         "public_ids": "public_ids",
     }
 
-    def __init__(self_, public_ids: Union[List[str], UnsetType] = unset, **kwargs):
+    def __init__(
+        self_,
+        force_delete_dependencies: Union[bool, UnsetType] = unset,
+        public_ids: Union[List[str], UnsetType] = unset,
+        **kwargs,
+    ):
         """
         A JSON list of the ID or IDs of the Synthetic tests that you want
         to delete.
 
+        :param force_delete_dependencies: Delete the Synthetic test even if it's referenced by other resources
+            (for example, SLOs and composite monitors).
+        :type force_delete_dependencies: bool, optional
+
         :param public_ids: An array of Synthetic test IDs you want to delete.
         :type public_ids: [str], optional
         """
+        if force_delete_dependencies is not unset:
+            kwargs["force_delete_dependencies"] = force_delete_dependencies
         if public_ids is not unset:
             kwargs["public_ids"] = public_ids
         super().__init__(kwargs)
