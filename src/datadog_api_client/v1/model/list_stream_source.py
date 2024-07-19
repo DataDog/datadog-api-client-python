b7ec845df7c26900ba0fc37cfe0cdaaa52b24fb5
ci.datadog-api-spec <packages@datadoghq.com>
2024-07-19 17:24:01 +0000

    Regenerate client from commit e7026d11 of spec repo

--- src/datadog_api_client/v1/model/list_stream_source.py
+++ src/datadog_api_client/v1/model/list_stream_source.py
@@ -16,7 +16,7 @@ class ListStreamSource(ModelSimple):
     """
     Source from which to query items to display in the stream.
 
-    :param value: If omitted defaults to "apm_issue_stream". Must be one of ["logs_stream", "audit_stream", "ci_pipeline_stream", "ci_test_stream", "rum_issue_stream", "apm_issue_stream", "trace_stream", "logs_issue_stream", "logs_pattern_stream", "logs_transaction_stream", "event_stream"].
+    :param value: If omitted defaults to "apm_issue_stream". Must be one of ["logs_stream", "audit_stream", "ci_pipeline_stream", "ci_test_stream", "rum_issue_stream", "apm_issue_stream", "trace_stream", "logs_issue_stream", "logs_pattern_stream", "logs_transaction_stream", "event_stream", "rum_stream"].
     :type value: str
     """
 
@@ -32,6 +32,7 @@ class ListStreamSource(ModelSimple):
         "logs_pattern_stream",
         "logs_transaction_stream",
         "event_stream",
+        "rum_stream",
     }
     LOGS_STREAM: ClassVar["ListStreamSource"]
     AUDIT_STREAM: ClassVar["ListStreamSource"]
@@ -44,6 +45,7 @@ class ListStreamSource(ModelSimple):
     LOGS_PATTERN_STREAM: ClassVar["ListStreamSource"]
     LOGS_TRANSACTION_STREAM: ClassVar["ListStreamSource"]
     EVENT_STREAM: ClassVar["ListStreamSource"]
+    RUM_STREAM: ClassVar["ListStreamSource"]
 
     @cached_property
     def openapi_types(_):
@@ -63,3 +65,4 @@ ListStreamSource.LOGS_ISSUE_STREAM = ListStreamSource("logs_issue_stream")
 ListStreamSource.LOGS_PATTERN_STREAM = ListStreamSource("logs_pattern_stream")
 ListStreamSource.LOGS_TRANSACTION_STREAM = ListStreamSource("logs_transaction_stream")
 ListStreamSource.EVENT_STREAM = ListStreamSource("event_stream")
+ListStreamSource.RUM_STREAM = ListStreamSource("rum_stream")
