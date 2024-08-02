76a8d8bb6a758dd2b12ad19581d0f015658e797c
ci.datadog-api-spec <packages@datadoghq.com>
2024-08-02 13:54:33 +0000

    Regenerate client from commit 74b2ffcb of spec repo

--- src/datadog_api_client/v2/model/role_relationships.py
+++ src/datadog_api_client/v2/model/role_relationships.py
@@ -15,42 +15,28 @@ from datadog_api_client.model_utils import (
 
 if TYPE_CHECKING:
     from datadog_api_client.v2.model.relationship_to_permissions import RelationshipToPermissions
-    from datadog_api_client.v2.model.relationship_to_users import RelationshipToUsers
 
 
 class RoleRelationships(ModelNormal):
     @cached_property
     def openapi_types(_):
         from datadog_api_client.v2.model.relationship_to_permissions import RelationshipToPermissions
-        from datadog_api_client.v2.model.relationship_to_users import RelationshipToUsers
 
         return {
             "permissions": (RelationshipToPermissions,),
-            "users": (RelationshipToUsers,),
         }
 
     attribute_map = {
         "permissions": "permissions",
-        "users": "users",
     }
 
-    def __init__(
-        self_,
-        permissions: Union[RelationshipToPermissions, UnsetType] = unset,
-        users: Union[RelationshipToUsers, UnsetType] = unset,
-        **kwargs,
-    ):
+    def __init__(self_, permissions: Union[RelationshipToPermissions, UnsetType] = unset, **kwargs):
         """
         Relationships of the role object.
 
         :param permissions: Relationship to multiple permissions objects.
         :type permissions: RelationshipToPermissions, optional
-
-        :param users: Relationship to users.
-        :type users: RelationshipToUsers, optional
         """
         if permissions is not unset:
             kwargs["permissions"] = permissions
-        if users is not unset:
-            kwargs["users"] = users
         super().__init__(kwargs)
