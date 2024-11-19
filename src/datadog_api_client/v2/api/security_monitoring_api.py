c38d6cc828f73ba254e3a331b00b14433fa8663a
ci.datadog-api-spec <packages@datadoghq.com>
2024-11-19 19:46:30 +0000

    Regenerate client from commit 04f6c81e of spec repo

--- src/datadog_api_client/v2/api/security_monitoring_api.py
+++ src/datadog_api_client/v2/api/security_monitoring_api.py
@@ -1386,6 +1386,8 @@ class SecurityMonitoringApi:
 
         Get a list of findings. These include both misconfigurations and identity risks.
 
+        **Note** : To filter and return only identity risks, add the following query parameter: ``?filter[tags]=dd_rule_type:ciem``
+
         **Filtering**
 
         Filters can be applied by appending query parameters to the URL.
