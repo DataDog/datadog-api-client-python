d5582cbc358125cab532519557a9275c54712665
ci.datadog-api-spec <packages@datadoghq.com>
2025-05-08 19:19:17 +0000

    Regenerate client from commit 2386b270 of spec repo

--- src/datadog_api_client/v2/model/metric_tag_configuration_attributes.py
+++ src/datadog_api_client/v2/model/metric_tag_configuration_attributes.py
@@ -59,22 +59,7 @@ class MetricTagConfigurationAttributes(ModelNormal):
         """
         Object containing the definition of a metric tag configuration attributes.
 
-        :param aggregations: A list of queryable aggregation combinations for a count, rate, or gauge metric.
-            By default, count and rate metrics require the (time: sum, space: sum) aggregation and
-            Gauge metrics require the (time: avg, space: avg) aggregation.
-            Additional time & space combinations are also available:
-
-            * time: avg, space: avg
-            * time: avg, space: max
-            * time: avg, space: min
-            * time: avg, space: sum
-            * time: count, space: sum
-            * time: max, space: max
-            * time: min, space: min
-            * time: sum, space: avg
-            * time: sum, space: sum
-
-            Can only be applied to non_distribution metrics that have a ``metric_type`` of ``count`` , ``rate`` , or ``gauge``.
+        :param aggregations: Deprecated. You no longer need to configure specific time and space aggregations for Metrics Without Limits.
         :type aggregations: MetricCustomAggregations, optional
 
         :param created_at: Timestamp when the tag configuration was created.
