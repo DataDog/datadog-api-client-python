df309d0a8e9b425cc99aff365746a6a999ee5bfe
ci.datadog-api-spec <packages@datadoghq.com>
2025-05-28 18:52:37 +0000

    Regenerate client from commit 26831bc8 of spec repo

--- examples/v2/microsoft-teams-integration/CreateWorkflowsWebhookHandle_1716851881.py
+++ examples/v2/microsoft-teams-integration/CreateWorkflowsWebhookHandle_1716851881.py
@@ -21,7 +21,7 @@ body = MicrosoftTeamsCreateWorkflowsWebhookHandleRequest(
     data=MicrosoftTeamsWorkflowsWebhookHandleRequestData(
         attributes=MicrosoftTeamsWorkflowsWebhookHandleRequestAttributes(
             name="Example-Microsoft-Teams-Integration",
-            url="https://fake.url.com",
+            url="https://example.logic.azure.com/workflows/123",
         ),
         type=MicrosoftTeamsWorkflowsWebhookHandleType.WORKFLOWS_WEBHOOK_HANDLE,
     ),
