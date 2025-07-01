ed5d427e16c2e652fe24769053d044f2e86edcb8
Anika Maskara <anika.maskara@datadoghq.com>
2025-07-01 15:00:29 -0400

    add fake app key

--- tests/test_async.py
+++ tests/test_async.py
@@ -14,6 +14,7 @@ from datadog_api_client.v1.api import dashboards_api, metrics_api
 async def test_error():
     configuration = Configuration()
     configuration.api_key["apiKeyAuth"] = "00000000000000000000000000000000"
+    configuration.api_key["appKeyAuth"] = "00000000000000000000000000000000"
 
     async with AsyncApiClient(configuration) as api_client:
         api_instance = metrics_api.MetricsApi(api_client)
