4cca1b1355feb10ddc1b54eea83cf64141a84f31
ci.datadog-api-spec <packages@datadoghq.com>
2025-07-02 12:39:26 +0000

    Regenerate client from commit 4fd1c189 of spec repo

---
+++ examples/v1/synthetics/SearchTests.py
@@ -0,0 +1,13 @@
+"""
+Search Synthetic tests returns "OK - Returns the list of Synthetic tests matching the search." response
+"""
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v1.api.synthetics_api import SyntheticsApi
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = SyntheticsApi(api_client)
+    response = api_instance.search_tests()
+
+    print(response)
