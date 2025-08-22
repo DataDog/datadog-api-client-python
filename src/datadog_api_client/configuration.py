985c8fefbb6502036a5b4f92b211fc199e044d6d
ci.datadog-api-spec <packages@datadoghq.com>
2025-08-22 14:59:17 +0000

    Regenerate client from commit a35d4f4 of spec repo

--- src/datadog_api_client/configuration.py
+++ src/datadog_api_client/configuration.py
@@ -312,6 +312,7 @@ class Configuration:
                 "v2.delete_scorecard_rule": False,
                 "v2.list_scorecard_outcomes": False,
                 "v2.list_scorecard_rules": False,
+                "v2.update_scorecard_outcomes_async": False,
                 "v2.update_scorecard_rule": False,
                 "v2.create_incident_service": False,
                 "v2.delete_incident_service": False,
