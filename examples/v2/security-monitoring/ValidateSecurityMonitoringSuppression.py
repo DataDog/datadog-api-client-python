52eae0f728ead4a6f935f799b1659a103aa3f3e4
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-02 19:10:01 +0000

    Regenerate client from commit dc3752a of spec repo

---
+++ examples/v2/security-monitoring/ValidateSecurityMonitoringSuppression.py
@@ -0,0 +1,34 @@
+"""
+Validate a suppression rule returns "OK" response
+"""
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v2.api.security_monitoring_api import SecurityMonitoringApi
+from datadog_api_client.v2.model.security_monitoring_suppression_type import SecurityMonitoringSuppressionType
+from datadog_api_client.v2.model.security_monitoring_suppression_update_attributes import (
+    SecurityMonitoringSuppressionUpdateAttributes,
+)
+from datadog_api_client.v2.model.security_monitoring_suppression_update_data import (
+    SecurityMonitoringSuppressionUpdateData,
+)
+from datadog_api_client.v2.model.security_monitoring_suppression_update_request import (
+    SecurityMonitoringSuppressionUpdateRequest,
+)
+
+body = SecurityMonitoringSuppressionUpdateRequest(
+    data=SecurityMonitoringSuppressionUpdateData(
+        attributes=SecurityMonitoringSuppressionUpdateAttributes(
+            data_exclusion_query="source:cloudtrail account_id:12345",
+            description="This rule suppresses low-severity signals in staging environments.",
+            enabled=True,
+            name="Custom suppression",
+            rule_query="type:log_detection source:cloudtrail",
+        ),
+        type=SecurityMonitoringSuppressionType.SUPPRESSIONS,
+    ),
+)
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = SecurityMonitoringApi(api_client)
+    api_instance.validate_security_monitoring_suppression(body=body)
