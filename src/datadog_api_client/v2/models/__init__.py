881369f81acf45e836e94519e44bc9aca015d85c
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-18 15:16:36 +0000

    Regenerate client from commit bd15e41 of spec repo

--- src/datadog_api_client/v2/models/__init__.py
+++ src/datadog_api_client/v2/models/__init__.py
@@ -2592,6 +2592,12 @@ from datadog_api_client.v2.model.observability_pipeline_open_search_destination 
 from datadog_api_client.v2.model.observability_pipeline_open_search_destination_type import (
     ObservabilityPipelineOpenSearchDestinationType,
 )
+from datadog_api_client.v2.model.observability_pipeline_opentelemetry_source import (
+    ObservabilityPipelineOpentelemetrySource,
+)
+from datadog_api_client.v2.model.observability_pipeline_opentelemetry_source_type import (
+    ObservabilityPipelineOpentelemetrySourceType,
+)
 from datadog_api_client.v2.model.observability_pipeline_parse_grok_processor import (
     ObservabilityPipelineParseGrokProcessor,
 )
@@ -6115,6 +6121,8 @@ __all__ = [
     "ObservabilityPipelineOcsfMappingLibrary",
     "ObservabilityPipelineOpenSearchDestination",
     "ObservabilityPipelineOpenSearchDestinationType",
+    "ObservabilityPipelineOpentelemetrySource",
+    "ObservabilityPipelineOpentelemetrySourceType",
     "ObservabilityPipelineParseGrokProcessor",
     "ObservabilityPipelineParseGrokProcessorRule",
     "ObservabilityPipelineParseGrokProcessorRuleMatchRule",
