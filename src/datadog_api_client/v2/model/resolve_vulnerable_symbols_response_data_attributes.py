738a7a336160cd2d599a2dcfe0d1d135954f066b
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-26 21:17:34 +0000

    Regenerate client from commit 2bf1795 of spec repo

---
+++ src/datadog_api_client/v2/model/resolve_vulnerable_symbols_response_data_attributes.py
@@ -0,0 +1,50 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019-Present Datadog, Inc.
+from __future__ import annotations
+
+from typing import List, Union, TYPE_CHECKING
+
+from datadog_api_client.model_utils import (
+    ModelNormal,
+    cached_property,
+    unset,
+    UnsetType,
+)
+
+
+if TYPE_CHECKING:
+    from datadog_api_client.v2.model.resolve_vulnerable_symbols_response_data_attributes_results_items import (
+        ResolveVulnerableSymbolsResponseDataAttributesResultsItems,
+    )
+
+
+class ResolveVulnerableSymbolsResponseDataAttributes(ModelNormal):
+    @cached_property
+    def openapi_types(_):
+        from datadog_api_client.v2.model.resolve_vulnerable_symbols_response_data_attributes_results_items import (
+            ResolveVulnerableSymbolsResponseDataAttributesResultsItems,
+        )
+
+        return {
+            "results": ([ResolveVulnerableSymbolsResponseDataAttributesResultsItems],),
+        }
+
+    attribute_map = {
+        "results": "results",
+    }
+
+    def __init__(
+        self_,
+        results: Union[List[ResolveVulnerableSymbolsResponseDataAttributesResultsItems], UnsetType] = unset,
+        **kwargs,
+    ):
+        """
+        The definition of ``ResolveVulnerableSymbolsResponseDataAttributes`` object.
+
+        :param results: The ``attributes`` ``results``.
+        :type results: [ResolveVulnerableSymbolsResponseDataAttributesResultsItems], optional
+        """
+        if results is not unset:
+            kwargs["results"] = results
+        super().__init__(kwargs)
