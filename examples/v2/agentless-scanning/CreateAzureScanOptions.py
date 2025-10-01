3abcdf56038d9b1c5a83ee35f8a7201702966ce1
ci.datadog-api-spec <packages@datadoghq.com>
2025-10-01 14:18:19 +0000

    Regenerate client from commit ba47c96 of spec repo

---
+++ examples/v2/agentless-scanning/CreateAzureScanOptions.py
@@ -0,0 +1,28 @@
+"""
+Create azure scan options returns "Created" response
+"""
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v2.api.agentless_scanning_api import AgentlessScanningApi
+from datadog_api_client.v2.model.azure_scan_options import AzureScanOptions
+from datadog_api_client.v2.model.azure_scan_options_data import AzureScanOptionsData
+from datadog_api_client.v2.model.azure_scan_options_data_attributes import AzureScanOptionsDataAttributes
+from datadog_api_client.v2.model.azure_scan_options_data_type import AzureScanOptionsDataType
+
+body = AzureScanOptions(
+    data=AzureScanOptionsData(
+        attributes=AzureScanOptionsDataAttributes(
+            vuln_containers_os=True,
+            vuln_host_os=True,
+        ),
+        id="12345678-90ab-cdef-1234-567890abcdef",
+        type=AzureScanOptionsDataType.AZURE_SCAN_OPTIONS,
+    ),
+)
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = AgentlessScanningApi(api_client)
+    response = api_instance.create_azure_scan_options(body=body)
+
+    print(response)
---
+++ src/datadog_api_client/v2/model/azure_scan_options.py
@@ -0,0 +1,42 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019-Present Datadog, Inc.
+from __future__ import annotations
+
+from typing import Union, TYPE_CHECKING
+
+from datadog_api_client.model_utils import (
+    ModelNormal,
+    cached_property,
+    unset,
+    UnsetType,
+)
+
+
+if TYPE_CHECKING:
+    from datadog_api_client.v2.model.azure_scan_options_data import AzureScanOptionsData
+
+
+class AzureScanOptions(ModelNormal):
+    @cached_property
+    def openapi_types(_):
+        from datadog_api_client.v2.model.azure_scan_options_data import AzureScanOptionsData
+
+        return {
+            "data": (AzureScanOptionsData,),
+        }
+
+    attribute_map = {
+        "data": "data",
+    }
+
+    def __init__(self_, data: Union[AzureScanOptionsData, UnsetType] = unset, **kwargs):
+        """
+        The definition of ``AzureScanOptions`` object.
+
+        :param data: The definition of ``AzureScanOptionsData`` object.
+        :type data: AzureScanOptionsData, optional
+        """
+        if data is not unset:
+            kwargs["data"] = data
+        super().__init__(kwargs)
