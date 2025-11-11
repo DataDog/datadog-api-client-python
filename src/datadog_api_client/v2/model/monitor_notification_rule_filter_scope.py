c9de0f7f96b22c039bffed6406defa6c2922acdf
ci.datadog-api-spec <packages@datadoghq.com>
2025-11-11 21:38:59 +0000

    Regenerate client from commit a1d10e5 of spec repo

---
+++ src/datadog_api_client/v2/model/monitor_notification_rule_filter_scope.py
@@ -0,0 +1,44 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019-Present Datadog, Inc.
+from __future__ import annotations
+
+
+from datadog_api_client.model_utils import (
+    ModelNormal,
+    cached_property,
+)
+
+
+class MonitorNotificationRuleFilterScope(ModelNormal):
+    validations = {
+        "scope": {
+            "max_length": 3000,
+            "min_length": 1,
+        },
+    }
+
+    @cached_property
+    def additional_properties_type(_):
+        return None
+
+    @cached_property
+    def openapi_types(_):
+        return {
+            "scope": (str,),
+        }
+
+    attribute_map = {
+        "scope": "scope",
+    }
+
+    def __init__(self_, scope: str, **kwargs):
+        """
+        Filter monitors by scope. Monitors must match the scope evaluation.
+
+        :param scope: The scope to which the monitor notification rule applies.
+        :type scope: str
+        """
+        super().__init__(kwargs)
+
+        self_.scope = scope
