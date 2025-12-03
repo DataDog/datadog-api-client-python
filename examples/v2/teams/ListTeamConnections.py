bcb23db7a92731eb32a7f9250d4493aa96717441
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-03 15:31:50 +0000

    Regenerate client from commit 4602cff of spec repo

--- examples/v2/teams/ListTeamConnections.py
+++ examples/v2/teams/ListTeamConnections.py
@@ -3,12 +3,12 @@ List team connections returns "OK" response
 """
 
 from datadog_api_client import ApiClient, Configuration
-from datadog_api_client.v2.api.team_connections_api import TeamConnectionsApi
+from datadog_api_client.v2.api.teams_api import TeamsApi
 
 configuration = Configuration()
 configuration.unstable_operations["list_team_connections"] = True
 with ApiClient(configuration) as api_client:
-    api_instance = TeamConnectionsApi(api_client)
+    api_instance = TeamsApi(api_client)
     response = api_instance.list_team_connections()
 
     print(response)
