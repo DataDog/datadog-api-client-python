b932374ebc58ddf8beec1cfbd9e12fdc3ed7f30e
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-05 13:25:40 +0000

    Regenerate client from commit 39fe4b2 of spec repo

---
+++ examples/v2/synthetics/SearchSuites.py
@@ -0,0 +1,13 @@
+"""
+Search Synthetics suites returns "OK" response
+"""
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v2.api.synthetics_api import SyntheticsApi
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = SyntheticsApi(api_client)
+    response = api_instance.search_suites()
+
+    print(response)
