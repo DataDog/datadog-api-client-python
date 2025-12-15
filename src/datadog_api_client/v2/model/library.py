4d2e7b2720c4e3a799965f5b7a29323a254621c2
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-15 10:18:00 +0000

    Regenerate client from commit fcf2546 of spec repo

--- src/datadog_api_client/v2/model/library.py
+++ src/datadog_api_client/v2/model/library.py
@@ -3,7 +3,7 @@
 # Copyright 2019-Present Datadog, Inc.
 from __future__ import annotations
 
-from typing import Union
+from typing import List, Union
 
 from datadog_api_client.model_utils import (
     ModelNormal,
@@ -17,18 +17,29 @@ class Library(ModelNormal):
     @cached_property
     def openapi_types(_):
         return {
+            "additional_names": ([str],),
             "name": (str,),
             "version": (str,),
         }
 
     attribute_map = {
+        "additional_names": "additional_names",
         "name": "name",
         "version": "version",
     }
 
-    def __init__(self_, name: str, version: Union[str, UnsetType] = unset, **kwargs):
+    def __init__(
+        self_,
+        name: str,
+        additional_names: Union[List[str], UnsetType] = unset,
+        version: Union[str, UnsetType] = unset,
+        **kwargs,
+    ):
         """
         Vulnerability library.
+
+        :param additional_names: Related library or package names (such as child packages or affected binary paths).
+        :type additional_names: [str], optional
 
         :param name: Vulnerability library name.
         :type name: str
@@ -36,6 +47,8 @@ class Library(ModelNormal):
         :param version: Vulnerability library version.
         :type version: str, optional
         """
+        if additional_names is not unset:
+            kwargs["additional_names"] = additional_names
         if version is not unset:
             kwargs["version"] = version
         super().__init__(kwargs)
