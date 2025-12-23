bef74e080d0503f86ec42263fab8e6f02128e4d3
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-23 15:17:19 +0000

    Regenerate client from commit d948812 of spec repo

--- src/datadog_api_client/v2/models/__init__.py
+++ src/datadog_api_client/v2/models/__init__.py
@@ -2997,6 +2997,9 @@ from datadog_api_client.v2.model.observability_pipeline_elasticsearch_destinatio
 from datadog_api_client.v2.model.observability_pipeline_elasticsearch_destination_api_version import (
     ObservabilityPipelineElasticsearchDestinationApiVersion,
 )
+from datadog_api_client.v2.model.observability_pipeline_elasticsearch_destination_data_stream import (
+    ObservabilityPipelineElasticsearchDestinationDataStream,
+)
 from datadog_api_client.v2.model.observability_pipeline_elasticsearch_destination_type import (
     ObservabilityPipelineElasticsearchDestinationType,
 )
@@ -7395,6 +7398,7 @@ __all__ = [
     "ObservabilityPipelineDedupeProcessorType",
     "ObservabilityPipelineElasticsearchDestination",
     "ObservabilityPipelineElasticsearchDestinationApiVersion",
+    "ObservabilityPipelineElasticsearchDestinationDataStream",
     "ObservabilityPipelineElasticsearchDestinationType",
     "ObservabilityPipelineEnrichmentTableFile",
     "ObservabilityPipelineEnrichmentTableFileEncoding",
