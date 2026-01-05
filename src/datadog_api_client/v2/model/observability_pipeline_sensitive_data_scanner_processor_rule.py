ff17c6872f735d4ccd5e038d5a7616a2e2ad95b4
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-05 14:49:55 +0000

    Regenerate client from commit 4ca5fe5 of spec repo

--- src/datadog_api_client/v2/model/observability_pipeline_sensitive_data_scanner_processor_rule.py
+++ src/datadog_api_client/v2/model/observability_pipeline_sensitive_data_scanner_processor_rule.py
@@ -106,8 +106,8 @@ class ObservabilityPipelineSensitiveDataScannerProcessorRule(ModelNormal):
             ObservabilityPipelineSensitiveDataScannerProcessorScopeExclude,
             ObservabilityPipelineSensitiveDataScannerProcessorScopeAll,
         ],
-        tags: List[str],
         keyword_options: Union[ObservabilityPipelineSensitiveDataScannerProcessorKeywordOptions, UnsetType] = unset,
+        tags: Union[List[str], UnsetType] = unset,
         **kwargs,
     ):
         """
@@ -129,14 +129,15 @@ class ObservabilityPipelineSensitiveDataScannerProcessorRule(ModelNormal):
         :type scope: ObservabilityPipelineSensitiveDataScannerProcessorScope
 
         :param tags: Tags assigned to this rule for filtering and classification.
-        :type tags: [str]
+        :type tags: [str], optional
         """
         if keyword_options is not unset:
             kwargs["keyword_options"] = keyword_options
+        if tags is not unset:
+            kwargs["tags"] = tags
         super().__init__(kwargs)
 
         self_.name = name
         self_.on_match = on_match
         self_.pattern = pattern
         self_.scope = scope
-        self_.tags = tags
