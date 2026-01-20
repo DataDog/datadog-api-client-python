d85318b36cff6f93f4c3204629aa0694cd0d8346
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-20 21:30:44 +0000

    Regenerate client from commit 5db4165 of spec repo

--- src/datadog_api_client/v1/api/monitors_api.py
+++ src/datadog_api_client/v1/api/monitors_api.py
@@ -404,6 +404,7 @@ class MonitorsApi:
         * database-monitoring: ``database-monitoring alert``
         * network-performance: ``network-performance alert``
         * cloud cost: ``cost alert``
+        * network-path: ``network-path alert``
 
         **Notes** :
 
@@ -604,6 +605,18 @@ class MonitorsApi:
           * for ``anomaly`` supports ``>=``
           * for ``forecast`` supports ``>``
 
+        * ``#`` an integer or decimal number used to set the threshold.
+
+        **Network Path Alert Query**
+
+        Example: ``network-path(query).index(index_name).rollup(rollup_method[, measure]).last(time_window) operator #``
+
+        * ``query`` The search query - following the `Log search syntax <https://docs.datadoghq.com/logs/search_syntax/>`_.
+        * ``index_name`` The data type to monitor on - supports ``netpath-path`` and ``netpath-hop``.
+        * ``rollup_method`` The stats roll-up method - supports ``count`` , ``avg`` , and ``cardinality``.
+        * ``measure`` For ``avg`` and cardinality ``rollup_method`` - specify the measure or the facet name you want to use.
+        * ``time_window`` #m (between 1 and 2880), #h (between 1 and 48).
+        * ``operator`` ``<`` , ``<=`` , ``>`` , ``>=`` , ``==`` , or ``!=``.
         * ``#`` an integer or decimal number used to set the threshold.
 
         :param body: Create a monitor request body.
