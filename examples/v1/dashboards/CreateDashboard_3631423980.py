95ce00f55322c2a9c71ce5e7b22aae41883636b8
ci.datadog-api-spec <packages@datadoghq.com>
2026-03-06 19:47:44 +0000

    Regenerate client from commit 7a70f56 of spec repo

--- examples/v1/dashboards/CreateDashboard_3631423980.py
+++ examples/v1/dashboards/CreateDashboard_3631423980.py
@@ -6,6 +6,7 @@ from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v1.api.dashboards_api import DashboardsApi
 from datadog_api_client.v1.model.dashboard import Dashboard
 from datadog_api_client.v1.model.dashboard_layout_type import DashboardLayoutType
+from datadog_api_client.v1.model.timeseries_request_style import TimeseriesRequestStyle
 from datadog_api_client.v1.model.timeseries_widget_definition import TimeseriesWidgetDefinition
 from datadog_api_client.v1.model.timeseries_widget_definition_type import TimeseriesWidgetDefinitionType
 from datadog_api_client.v1.model.timeseries_widget_request import TimeseriesWidgetRequest
@@ -13,7 +14,6 @@ from datadog_api_client.v1.model.widget import Widget
 from datadog_api_client.v1.model.widget_display_type import WidgetDisplayType
 from datadog_api_client.v1.model.widget_line_type import WidgetLineType
 from datadog_api_client.v1.model.widget_line_width import WidgetLineWidth
-from datadog_api_client.v1.model.widget_request_style import WidgetRequestStyle
 
 body = Dashboard(
     layout_type=DashboardLayoutType.ORDERED,
@@ -25,7 +25,7 @@ body = Dashboard(
                 requests=[
                     TimeseriesWidgetRequest(
                         q="avg:system.cpu.user{*} by {host}",
-                        style=WidgetRequestStyle(
+                        style=TimeseriesRequestStyle(
                             palette="dog_classic",
                             line_type=WidgetLineType.SOLID,
                             line_width=WidgetLineWidth.NORMAL,
