8e6772df5f918462e0d54d7a8fac3f01d03ff54e
ci.datadog-api-spec <packages@datadoghq.com>
2026-05-06 07:34:06 +0000

    Regenerate client from commit a8c070f of spec repo

--- src/datadog_api_client/v2/model/azure_scan_options_data_attributes.py
+++ src/datadog_api_client/v2/model/azure_scan_options_data_attributes.py
@@ -17,17 +17,20 @@ class AzureScanOptionsDataAttributes(ModelNormal):
     @cached_property
     def openapi_types(_):
         return {
+            "compliance_host": (bool,),
             "vuln_containers_os": (bool,),
             "vuln_host_os": (bool,),
         }
 
     attribute_map = {
+        "compliance_host": "compliance_host",
         "vuln_containers_os": "vuln_containers_os",
         "vuln_host_os": "vuln_host_os",
     }
 
     def __init__(
         self_,
+        compliance_host: Union[bool, UnsetType] = unset,
         vuln_containers_os: Union[bool, UnsetType] = unset,
         vuln_host_os: Union[bool, UnsetType] = unset,
         **kwargs,
@@ -35,12 +38,17 @@ class AzureScanOptionsDataAttributes(ModelNormal):
         """
         Attributes for Azure scan options configuration.
 
+        :param compliance_host: Indicates whether host compliance scanning is enabled.
+        :type compliance_host: bool, optional
+
         :param vuln_containers_os: Indicates if scanning for vulnerabilities in containers is enabled.
         :type vuln_containers_os: bool, optional
 
         :param vuln_host_os: Indicates if scanning for vulnerabilities in hosts is enabled.
         :type vuln_host_os: bool, optional
         """
+        if compliance_host is not unset:
+            kwargs["compliance_host"] = compliance_host
         if vuln_containers_os is not unset:
             kwargs["vuln_containers_os"] = vuln_containers_os
         if vuln_host_os is not unset:
