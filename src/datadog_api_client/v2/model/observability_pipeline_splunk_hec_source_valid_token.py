2c4fdd74cd8a9c4766ccfad5b468ed3d9a0f57b7
ci.datadog-api-spec <packages@datadoghq.com>
2026-05-19 08:59:57 +0000

    Regenerate client from commit 465019e of spec repo

---
+++ src/datadog_api_client/v2/model/observability_pipeline_splunk_hec_source_valid_token.py
@@ -0,0 +1,72 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019-Present Datadog, Inc.
+from __future__ import annotations
+
+from typing import Union, TYPE_CHECKING
+
+from datadog_api_client.model_utils import (
+    ModelNormal,
+    cached_property,
+    unset,
+    UnsetType,
+)
+
+
+if TYPE_CHECKING:
+    from datadog_api_client.v2.model.observability_pipeline_source_valid_token_field_to_add import (
+        ObservabilityPipelineSourceValidTokenFieldToAdd,
+    )
+
+
+class ObservabilityPipelineSplunkHecSourceValidToken(ModelNormal):
+    validations = {
+        "token_key": {},
+    }
+
+    @cached_property
+    def openapi_types(_):
+        from datadog_api_client.v2.model.observability_pipeline_source_valid_token_field_to_add import (
+            ObservabilityPipelineSourceValidTokenFieldToAdd,
+        )
+
+        return {
+            "enabled": (bool,),
+            "field_to_add": (ObservabilityPipelineSourceValidTokenFieldToAdd,),
+            "token_key": (str,),
+        }
+
+    attribute_map = {
+        "enabled": "enabled",
+        "field_to_add": "field_to_add",
+        "token_key": "token_key",
+    }
+
+    def __init__(
+        self_,
+        token_key: str,
+        enabled: Union[bool, UnsetType] = unset,
+        field_to_add: Union[ObservabilityPipelineSourceValidTokenFieldToAdd, UnsetType] = unset,
+        **kwargs,
+    ):
+        """
+        An accepted HEC token used to authenticate incoming Splunk HEC requests.
+
+        :param enabled: Indicates whether this token is currently accepted. Disabled tokens are rejected without
+            being removed from the configuration.
+        :type enabled: bool, optional
+
+        :param field_to_add: An optional metadata field that is attached to every event authenticated by the
+            associated token. Both ``key`` and ``value`` must match ``^[A-Za-z0-9_]+$``.
+        :type field_to_add: ObservabilityPipelineSourceValidTokenFieldToAdd, optional
+
+        :param token_key: Name of the environment variable or secret that holds the expected HEC token value.
+        :type token_key: str
+        """
+        if enabled is not unset:
+            kwargs["enabled"] = enabled
+        if field_to_add is not unset:
+            kwargs["field_to_add"] = field_to_add
+        super().__init__(kwargs)
+
+        self_.token_key = token_key
