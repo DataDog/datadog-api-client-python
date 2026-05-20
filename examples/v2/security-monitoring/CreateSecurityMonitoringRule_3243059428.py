893ed16c166df1334010f6550f7a2ab9d2132855
ci.datadog-api-spec <packages@datadoghq.com>
2026-05-20 15:55:50 +0000

    Regenerate client from commit 92edb47 of spec repo

---
+++ examples/v2/security-monitoring/CreateSecurityMonitoringRule_3243059428.py
@@ -0,0 +1,73 @@
+"""
+Create a detection rule with type 'impossible_travel' and baselineUserLocationsDuration returns "OK" response
+"""
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v2.api.security_monitoring_api import SecurityMonitoringApi
+from datadog_api_client.v2.model.security_monitoring_rule_case_create import SecurityMonitoringRuleCaseCreate
+from datadog_api_client.v2.model.security_monitoring_rule_detection_method import SecurityMonitoringRuleDetectionMethod
+from datadog_api_client.v2.model.security_monitoring_rule_evaluation_window import (
+    SecurityMonitoringRuleEvaluationWindow,
+)
+from datadog_api_client.v2.model.security_monitoring_rule_impossible_travel_options import (
+    SecurityMonitoringRuleImpossibleTravelOptions,
+)
+from datadog_api_client.v2.model.security_monitoring_rule_keep_alive import SecurityMonitoringRuleKeepAlive
+from datadog_api_client.v2.model.security_monitoring_rule_max_signal_duration import (
+    SecurityMonitoringRuleMaxSignalDuration,
+)
+from datadog_api_client.v2.model.security_monitoring_rule_options import SecurityMonitoringRuleOptions
+from datadog_api_client.v2.model.security_monitoring_rule_query_aggregation import (
+    SecurityMonitoringRuleQueryAggregation,
+)
+from datadog_api_client.v2.model.security_monitoring_rule_severity import SecurityMonitoringRuleSeverity
+from datadog_api_client.v2.model.security_monitoring_rule_type_create import SecurityMonitoringRuleTypeCreate
+from datadog_api_client.v2.model.security_monitoring_standard_rule_create_payload import (
+    SecurityMonitoringStandardRuleCreatePayload,
+)
+from datadog_api_client.v2.model.security_monitoring_standard_rule_query import SecurityMonitoringStandardRuleQuery
+
+body = SecurityMonitoringStandardRuleCreatePayload(
+    queries=[
+        SecurityMonitoringStandardRuleQuery(
+            aggregation=SecurityMonitoringRuleQueryAggregation.GEO_DATA,
+            group_by_fields=[
+                "@usr.id",
+            ],
+            distinct_fields=[],
+            metric="@network.client.geoip",
+            query="*",
+        ),
+    ],
+    cases=[
+        SecurityMonitoringRuleCaseCreate(
+            name="",
+            status=SecurityMonitoringRuleSeverity.INFO,
+            notifications=[],
+        ),
+    ],
+    has_extended_title=True,
+    message="test",
+    is_enabled=True,
+    options=SecurityMonitoringRuleOptions(
+        max_signal_duration=SecurityMonitoringRuleMaxSignalDuration.ONE_DAY,
+        evaluation_window=SecurityMonitoringRuleEvaluationWindow.FIFTEEN_MINUTES,
+        keep_alive=SecurityMonitoringRuleKeepAlive.ONE_HOUR,
+        detection_method=SecurityMonitoringRuleDetectionMethod.IMPOSSIBLE_TRAVEL,
+        impossible_travel_options=SecurityMonitoringRuleImpossibleTravelOptions(
+            baseline_user_locations=True,
+            baseline_user_locations_duration=7,
+        ),
+    ),
+    name="Example-Security-Monitoring",
+    type=SecurityMonitoringRuleTypeCreate.LOG_DETECTION,
+    tags=[],
+    filters=[],
+)
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = SecurityMonitoringApi(api_client)
+    response = api_instance.create_security_monitoring_rule(body=body)
+
+    print(response)
--- src/datadog_api_client/v2/model/security_monitoring_rule_impossible_travel_options.py
+++ src/datadog_api_client/v2/model/security_monitoring_rule_impossible_travel_options.py
@@ -8,30 +8,50 @@ from typing import Union
 from datadog_api_client.model_utils import (
     ModelNormal,
     cached_property,
+    none_type,
     unset,
     UnsetType,
 )
 
 
 class SecurityMonitoringRuleImpossibleTravelOptions(ModelNormal):
+    validations = {
+        "baseline_user_locations_duration": {
+            "inclusive_maximum": 30,
+            "inclusive_minimum": 1,
+        },
+    }
+
     @cached_property
     def openapi_types(_):
         return {
             "baseline_user_locations": (bool,),
+            "baseline_user_locations_duration": (int,),
         }
 
     attribute_map = {
         "baseline_user_locations": "baselineUserLocations",
+        "baseline_user_locations_duration": "baselineUserLocationsDuration",
     }
 
-    def __init__(self_, baseline_user_locations: Union[bool, UnsetType] = unset, **kwargs):
+    def __init__(
+        self_,
+        baseline_user_locations: Union[bool, UnsetType] = unset,
+        baseline_user_locations_duration: Union[int, none_type, UnsetType] = unset,
+        **kwargs,
+    ):
         """
         Options on impossible travel detection method.
 
         :param baseline_user_locations: If true, signals are suppressed for the first 24 hours. In that time, Datadog learns the user's regular
             access locations. This can be helpful to reduce noise and infer VPN usage or credentialed API access.
         :type baseline_user_locations: bool, optional
+
+        :param baseline_user_locations_duration: The duration in days during which Datadog learns the user's regular access locations. After this period, signals are generated for accesses from unknown locations.
+        :type baseline_user_locations_duration: int, none_type, optional
         """
         if baseline_user_locations is not unset:
             kwargs["baseline_user_locations"] = baseline_user_locations
+        if baseline_user_locations_duration is not unset:
+            kwargs["baseline_user_locations_duration"] = baseline_user_locations_duration
         super().__init__(kwargs)
