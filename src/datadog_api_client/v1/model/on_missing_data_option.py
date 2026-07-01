85cd67b2cc8eb83c91c0ebac5f9824cf6afcc159
ci.datadog-api-spec <packages@datadoghq.com>
2026-07-01 17:31:37 +0000

    Regenerate client from commit a88bcc5 of spec repo

--- src/datadog_api_client/v1/model/on_missing_data_option.py
+++ src/datadog_api_client/v1/model/on_missing_data_option.py
@@ -18,7 +18,8 @@ class OnMissingDataOption(ModelSimple):
         The default option results in different behavior depending on the monitor query type.
         For monitors using Count queries, an empty monitor evaluation is treated as 0 and is compared to the threshold conditions.
         For monitors using any query type other than Count, for example Gauge, Measure, or Rate, the monitor shows the last known status.
-        This option is only available for APM Trace Analytics, Audit Trail, CI, Error Tracking, Event, Logs, and RUM monitors.
+        This option is available for APM Trace Analytics, Audit Trail, CI, Error Tracking, Event, Logs, and RUM monitors.
+        The option is also required for metric monitors that use `scheduling_options.custom_schedule`.
 
     :param value: Must be one of ["default", "show_no_data", "show_and_notify_no_data", "resolve"].
     :type value: str
