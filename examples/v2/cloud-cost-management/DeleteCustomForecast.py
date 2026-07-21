8cf8214e1c108554508a592c4cdc6766e9973c69
ci.datadog-api-spec <packages@datadoghq.com>
2026-07-21 16:05:01 +0000

    Regenerate client from commit 025f88b of spec repo

--- examples/v2/cloud-cost-management/DeleteCustomForecast.py
+++ examples/v2/cloud-cost-management/DeleteCustomForecast.py
@@ -6,7 +6,6 @@ from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v2.api.cloud_cost_management_api import CloudCostManagementApi
 
 configuration = Configuration()
-configuration.unstable_operations["delete_custom_forecast"] = True
 with ApiClient(configuration) as api_client:
     api_instance = CloudCostManagementApi(api_client)
     api_instance.delete_custom_forecast(
--- examples/v2/cloud-cost-management/UpsertCustomForecast.py
+++ examples/v2/cloud-cost-management/UpsertCustomForecast.py
@@ -36,7 +36,6 @@ body = CustomForecastUpsertRequest(
 )
 
 configuration = Configuration()
-configuration.unstable_operations["upsert_custom_forecast"] = True
 with ApiClient(configuration) as api_client:
     api_instance = CloudCostManagementApi(api_client)
     response = api_instance.upsert_custom_forecast(body=body)
