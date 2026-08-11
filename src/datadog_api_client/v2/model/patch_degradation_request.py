b72137535dd6a9923a9d190975dc3e8431c0fd92
ci.datadog-api-spec <packages@datadoghq.com>
2026-08-11 16:16:27 +0000

    Regenerate client from commit a385d26 of spec repo

--- src/datadog_api_client/v2/model/patch_degradation_request.py
+++ src/datadog_api_client/v2/model/patch_degradation_request.py
@@ -15,28 +15,42 @@ from datadog_api_client.model_utils import (
 
 if TYPE_CHECKING:
     from datadog_api_client.v2.model.patch_degradation_request_data import PatchDegradationRequestData
+    from datadog_api_client.v2.model.degradation_request_meta import DegradationRequestMeta
 
 
 class PatchDegradationRequest(ModelNormal):
     @cached_property
     def openapi_types(_):
         from datadog_api_client.v2.model.patch_degradation_request_data import PatchDegradationRequestData
+        from datadog_api_client.v2.model.degradation_request_meta import DegradationRequestMeta
 
         return {
             "data": (PatchDegradationRequestData,),
+            "meta": (DegradationRequestMeta,),
         }
 
     attribute_map = {
         "data": "data",
+        "meta": "meta",
     }
 
-    def __init__(self_, data: Union[PatchDegradationRequestData, UnsetType] = unset, **kwargs):
+    def __init__(
+        self_,
+        data: Union[PatchDegradationRequestData, UnsetType] = unset,
+        meta: Union[DegradationRequestMeta, UnsetType] = unset,
+        **kwargs,
+    ):
         """
         Request object for updating a degradation.
 
         :param data: The data object for updating a degradation.
         :type data: PatchDegradationRequestData, optional
+
+        :param meta: The supported metadata for a degradation request.
+        :type meta: DegradationRequestMeta, optional
         """
         if data is not unset:
             kwargs["data"] = data
+        if meta is not unset:
+            kwargs["meta"] = meta
         super().__init__(kwargs)
