7d06312c098ab2e320630165c95bd314a15b78b4
ci.datadog-api-spec <packages@datadoghq.com>
2026-08-12 08:05:48 +0000

    Regenerate client from commit b567f51 of spec repo

--- src/datadog_api_client/v1/model/monitor_type.py
+++ src/datadog_api_client/v1/model/monitor_type.py
@@ -16,7 +16,7 @@ class MonitorType(ModelSimple):
     """
     The type of the monitor. For more information about `type`, see the [monitor options](https://docs.datadoghq.com/monitors/guide/monitor_api_options/) docs.
 
-    :param value: Must be one of ["composite", "event alert", "log alert", "metric alert", "process alert", "query alert", "rum alert", "service check", "synthetics alert", "trace-analytics alert", "slo alert", "event-v2 alert", "audit alert", "ci-pipelines alert", "ci-tests alert", "error-tracking alert", "database-monitoring alert", "network-performance alert", "cost alert", "data-quality alert", "network-path alert", "data-jobs alert"].
+    :param value: Must be one of ["composite", "event alert", "log alert", "metric alert", "process alert", "query alert", "rum alert", "service check", "synthetics alert", "trace-analytics alert", "slo alert", "event-v2 alert", "audit alert", "ci-pipelines alert", "ci-tests alert", "error-tracking alert", "database-monitoring alert", "network-performance alert", "cost alert", "data-quality alert", "network-path alert", "data-jobs alert", "llm-observability alert"].
     :type value: str
     """
 
@@ -43,6 +43,7 @@ class MonitorType(ModelSimple):
         "data-quality alert",
         "network-path alert",
         "data-jobs alert",
+        "llm-observability alert",
     }
     COMPOSITE: ClassVar["MonitorType"]
     EVENT_ALERT: ClassVar["MonitorType"]
@@ -66,6 +67,7 @@ class MonitorType(ModelSimple):
     DATA_QUALITY_ALERT: ClassVar["MonitorType"]
     NETWORK_PATH_ALERT: ClassVar["MonitorType"]
     DATA_JOBS_ALERT: ClassVar["MonitorType"]
+    LLM_OBSERVABILITY_ALERT: ClassVar["MonitorType"]
 
     @cached_property
     def openapi_types(_):
@@ -96,3 +98,4 @@ MonitorType.COST_ALERT = MonitorType("cost alert")
 MonitorType.DATA_QUALITY_ALERT = MonitorType("data-quality alert")
 MonitorType.NETWORK_PATH_ALERT = MonitorType("network-path alert")
 MonitorType.DATA_JOBS_ALERT = MonitorType("data-jobs alert")
+MonitorType.LLM_OBSERVABILITY_ALERT = MonitorType("llm-observability alert")
