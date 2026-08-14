2cabb2dd8bcad09eb8df8ebd243317b391079957
ci.datadog-api-spec <packages@datadoghq.com>
2026-08-14 13:59:32 +0000

    Regenerate client from commit 22f067f of spec repo

--- src/datadog_api_client/v2/api/llm_observability_api.py
+++ src/datadog_api_client/v2/api/llm_observability_api.py
@@ -2269,7 +2269,8 @@ class LLMObservabilityApi:
     ) -> LLMObsAnnotationQueueResponse:
         """Create an LLM Observability annotation queue.
 
-        Create an annotation queue. The ``name`` and ``project_id`` fields are required.
+        Create an annotation queue. The ``name`` field is required.
+        Optionally associate the queue with a project by providing ``project_id``.
         An optional ``annotation_schema`` can be provided to define the labels for the queue.
         Fields such as ``created_by`` , ``owned_by`` , ``created_at`` , ``modified_by`` ,
         and ``modified_at`` are inferred by the backend.
