b8131e9ef7ed02714fc4fbc042f46c6ef35a0703
ci.datadog-api-spec <packages@datadoghq.com>
2026-08-27 10:17:11 +0000

    Regenerate client from commit 396c2b3 of spec repo

--- src/datadog_api_client/v2/models/__init__.py
+++ src/datadog_api_client/v2/models/__init__.py
@@ -6633,6 +6633,9 @@ from datadog_api_client.v2.model.observability_pipeline_splunk_hec_destination i
 from datadog_api_client.v2.model.observability_pipeline_splunk_hec_destination_encoding import (
     ObservabilityPipelineSplunkHecDestinationEncoding,
 )
+from datadog_api_client.v2.model.observability_pipeline_splunk_hec_destination_endpoint_target import (
+    ObservabilityPipelineSplunkHecDestinationEndpointTarget,
+)
 from datadog_api_client.v2.model.observability_pipeline_splunk_hec_destination_token_strategy import (
     ObservabilityPipelineSplunkHecDestinationTokenStrategy,
 )
@@ -15807,6 +15810,7 @@ __all__ = [
     "ObservabilityPipelineSplitArrayProcessorType",
     "ObservabilityPipelineSplunkHecDestination",
     "ObservabilityPipelineSplunkHecDestinationEncoding",
+    "ObservabilityPipelineSplunkHecDestinationEndpointTarget",
     "ObservabilityPipelineSplunkHecDestinationTokenStrategy",
     "ObservabilityPipelineSplunkHecDestinationType",
     "ObservabilityPipelineSplunkHecMetricsDestination",
