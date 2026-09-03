c99f5f4991e3a8add0adc8517b0ae455d471f2ba
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-03 16:33:48 +0000

    Regenerate client from commit 05e7bdc of spec repo

---
+++ examples/v2/observability-pipelines/GetPipeline_28187501.py
@@ -0,0 +1,19 @@
+"""
+Get a pipeline preserves end-to-end acknowledgements returns "OK" response
+"""
+
+from os import environ
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v2.api.observability_pipelines_api import ObservabilityPipelinesApi
+
+# there is a valid "pipeline with end-to-end acknowledgements" in the system
+PIPELINE_WITH_END_TO_END_ACKNOWLEDGEMENTS_DATA_ID = environ["PIPELINE_WITH_END_TO_END_ACKNOWLEDGEMENTS_DATA_ID"]
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = ObservabilityPipelinesApi(api_client)
+    response = api_instance.get_pipeline(
+        pipeline_id=PIPELINE_WITH_END_TO_END_ACKNOWLEDGEMENTS_DATA_ID,
+    )
+
+    print(response)
